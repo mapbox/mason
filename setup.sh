8b9595717d3074894524b9487911f76fc91f59b5
Konstantin Käfer <mail@kkaefer.com>
2014-10-14 14:45:25 +0200

    initial version

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+MASON_NAME=libuv
+MASON_VERSION=0.11.29
+MASON_LIB_FILE=lib/libuv.a
+
+. /usr/local/share/mason/mason.sh
+
+function mason_load_source {
+    mason_download \
+        https://github.com/joyent/libuv/archive/v0.11.29.tar.gz \
+        5bf49a8652f680557cbaf335a160187b2da3bf7f
+
+    mason_extract_tar_gz
+
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/libuv-${MASON_VERSION}
+}
+
+function mason_prepare_compile {
+    ./autogen.sh
+}
+
+function mason_compile {
+    ./configure \
+        --prefix=${MASON_PREFIX} \
+        ${MASON_HOST_ARG} \
+        --enable-static \
+        --disable-shared \
+        --disable-dependency-tracking
+
+    make install -j${MASON_CONCURRENCY}
+}
+
+function mason_clean {
+    make clean
+}
+
+mason_run $1

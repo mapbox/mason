3cdfac5c1405176149a0ce90c68542a45ae492cc
Konstantin Käfer <mail@kkaefer.com>
2014-10-17 11:27:13 +0200

    initial version

---
+++ script.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+MASON_NAME=libpng
+MASON_VERSION=1.6.13
+MASON_LIB_FILE=lib/libpng.a
+MASON_PKGCONFIG_FILE=lib/pkgconfig/libpng.pc
+
+. ~/.mason/mason.sh
+
+function mason_load_source {
+    mason_download \
+        http://prdownloads.sourceforge.net/libpng/libpng-1.6.13.tar.gz?download \
+        7e0365a970f8c032f8b6dcedcca77e041f149fe6
+
+    mason_extract_tar_gz
+
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/libpng-${MASON_VERSION}
+}
+
+function mason_compile {
+    ./configure \
+        --prefix=${MASON_PREFIX} \
+        ${MASON_HOST_ARG} \
+        --enable-static \
+        --disable-shared \
+        --disable-dependency-tracking
+
+    make install -j${MASON_CONCURRENCY}
+}
+
+function mason_clean {
+    make clean
+}
+
+mason_run "$@"

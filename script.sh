f0bfd10afb6abd6424ec02744ede60b1e07b1732
Konstantin Käfer <mail@kkaefer.com>
2014-10-17 04:21:09 -0700

    initial version of libcurl-7.38.0

---
+++ script.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+MASON_NAME=libcurl
+MASON_VERSION=7.38.0
+MASON_LIB_FILE=lib/libcurl.a
+MASON_PKGCONFIG_FILE=lib/pkgconfig/libcurl.pc
+
+. ~/.mason/mason.sh
+
+~/.mason/mason install openssl 1.0.1i
+MASON_OPENSSL=`~/.mason/mason prefix openssl 1.0.1i`
+
+function mason_load_source {
+    mason_download \
+        http://curl.haxx.se/download/curl-7.38.0.tar.gz \
+        5463f1b9dc807e4ae8be2ef4ed57e67f677f4426
+
+    mason_extract_tar_gz
+
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/curl-${MASON_VERSION}
+}
+
+function mason_prepare_compile {
+    if [ ${MASON_PLATFORM} = 'linux' ]; then
+        LIBS="-ldl ${LIBS=}"
+    fi
+}
+
+function mason_compile {
+    LIBS="${LIBS=}" ./configure \
+        --prefix=${MASON_PREFIX} \
+        ${MASON_HOST_ARG} \
+        --enable-static \
+        --disable-shared \
+        --with-pic \
+        --enable-manual \
+        --with-ssl=${MASON_OPENSSL} \
+        --without-ca-bundle \
+        --without-ca-path \
+        --without-darwinssl \
+        --without-gnutls \
+        --without-polarssl \
+        --without-cyassl \
+        --without-nss \
+        --without-axtls \
+        --without-libmetalink \
+        --without-libssh2 \
+        --without-librtmp \
+        --without-winidn \
+        --without-libidn \
+        --without-nghttp2 \
+        --disable-ldap \
+        --disable-ldaps \
+        --disable-ldap \
+        --disable-ftp \
+        --disable-file \
+        --disable-rtsp \
+        --disable-proxy \
+        --disable-dict \
+        --disable-telnet \
+        --disable-tftp \
+        --disable-pop3 \
+        --disable-imap \
+        --disable-smtp \
+        --disable-gopher \
+        --disable-libcurl-option \
+        --disable-sspi \
+        --disable-crypto-auth \
+        --disable-ntlm-wb \
+        --disable-tls-srp \
+        --disable-cookies
+
+    make -j${MASON_CONCURRENCY}
+    make install
+}
+
+function mason_clean {
+    make clean
+}
+
+mason_run "$@"

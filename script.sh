cc094230c2003700804c1c3aa66f5b47e4911a75
Konstantin Käfer <mail@kkaefer.com>
2014-10-21 02:21:41 -0700

    initial version of libpng-system

---
+++ script.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+MASON_NAME=libpng
+MASON_VERSION=system
+MASON_SYSTEM_PACKAGE=true
+
+. ~/.mason/mason.sh
+
+if [ ! $(pkg-config libpng --exists; echo $?) = 0 ]; then
+    mason_error "Cannot find libpng with pkg-config"
+    exit 1
+fi
+
+function mason_system_version {
+    mkdir -p "${MASON_PREFIX}"
+    cd "${MASON_PREFIX}"
+    if [ ! -f version ]; then
+        echo "#include <png.h>
+#include <stdio.h>
+#include <assert.h>
+int main() {
+    assert(PNG_LIBPNG_VER == png_access_version_number());
+    printf(\"%s\", PNG_LIBPNG_VER_STRING);
+    return 0;
+}
+" > version.c && ${CC:-cc} version.c $(mason_cflags) $(mason_ldflags) -o version
+    fi
+    ./version
+}
+
+function mason_compile {
+    :
+}
+
+function mason_cflags {
+    pkg-config libpng --cflags
+}
+
+function mason_ldflags {
+    pkg-config libpng --libs
+}
+
+mason_run "$@"

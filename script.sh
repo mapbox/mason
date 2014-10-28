598007b560cc6bc337c205dc8418d9c0e093d775
Konstantin Käfer <mail@kkaefer.com>
2014-10-28 10:51:49 -0400

    add correct include paths and cflags

--- script.sh
+++ script.sh
@@ -19,10 +19,12 @@ if [[ ${MASON_PLATFORM} = 'osx' || ${MASON_PLATFORM} = 'ios' ]]; then
         exit 1
     fi
 
+    MASON_INCLUDE_PATHS=
     MASON_CFLAGS=
     MASON_LDFLAGS=-lcurl
 else
-    MASON_CFLAGS=`pkg-config libcurl --cflags`
+    MASON_INCLUDE_PATHS=`pkg-config libcurl --cflags-only-I`
+    MASON_CFLAGS=`pkg-config libcurl --cflags-only-other`
     MASON_LDFLAGS=`pkg-config libcurl --libs`
 fi
 
@@ -43,6 +45,10 @@ int main() {
 
 function mason_compile {
     :
+}
+
+function mason_include_paths {
+    echo ${MASON_INCLUDE_PATHS}
 }
 
 function mason_cflags {

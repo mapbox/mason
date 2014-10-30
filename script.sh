643f14dbeb5c479e65ba98c441d59c8ba57d954a
Konstantin Käfer <mail@kkaefer.com>
2014-10-30 16:10:20 -0700

    initial version of mesa-10.3.1

---
+++ script.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+MASON_NAME=mesa
+MASON_VERSION=10.3.1
+MASON_LIB_FILE=lib/libGL.a
+MASON_PKGCONFIG_FILE=lib/pkgconfig/gl.pc
+
+. ~/.mason/mason.sh
+
+function mason_load_source {
+    mason_download \
+        http://ftp.de.debian.org/debian/pool/main/m/mesa/mesa_10.3.1.orig.tar.gz \
+        f823d156faf5a786b4c3d038094c38dd04b45c49
+
+    mason_extract_tar_gz
+
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/Mesa-10.3.1
+}
+
+function mason_compile {
+    ./autogen.sh \
+        --prefix=${MASON_PREFIX} \
+        ${MASON_HOST_ARG} \
+        --enable-static \
+        --enable-shared \
+        --with-gallium-drivers=svga,swrast \
+        --disable-dri \
+        --enable-xlib-glx \
+        --enable-glx-tls \
+        --with-llvm-prefix=/usr/lib/llvm-3.4 \
+        --without-va
+
+    make install
+}
+
+function mason_strip_ldflags {
+    shift # -L...
+    shift # -luv
+    echo "$@"
+}
+
+function mason_ldflags {
+    mason_strip_ldflags $(`mason_pkgconfig` --static --libs)
+}
+
+function mason_clean {
+    make clean
+}
+
+mason_run "$@"

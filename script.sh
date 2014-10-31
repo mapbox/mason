903c661d467132eb072f8b37abc05310b60db607
artemp <artem@mapnik.org>
2014-10-31 14:29:28 -0400

    initial version of nunicode-1.4

---
+++ script.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+MASON_NAME=nunicode
+MASON_VERSION=1.4
+MASON_LIB_FILE=lib/libnu.a
+MASON_PKGCONFIG_FILE=lib/pkgconfig/nu.pc
+
+. ~/.mason/mason.sh
+
+function mason_load_source {
+    mason_download \
+    https://bitbucket.org/alekseyt/nunicode/get/1.4.tar.bz2 \
+    0874ecc1e600f2ee29d689c9f32f92558610acee
+
+    mason_extract_tar_bz2
+
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/alekseyt-nunicode-4a0e4773e91a
+}
+
+function mason_compile {
+    mkdir -p build-dir
+    cd build-dir
+    cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=${MASON_PREFIX}
+    make
+    make install
+}
+
+function mason_cflags {
+    echo -I${MASON_PREFIX}/include
+}
+
+function mason_ldflags {
+    echo -L${MASON_PREFIX}/lib -lnu
+}
+
+function mason_clean {
+    make clean
+}
+
+mason_run "$@"

bfb652273320a0153e9bb4485730687f5c945322
Konstantin Käfer <mail@kkaefer.com>
2014-11-04 16:32:45 +0100

    do a header-only build of boost headers and only unzip the ones we need

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@
 
 MASON_NAME=boost
 MASON_VERSION=1.57.0
+MASON_HEADER_ONLY=true
 
 . ${MASON_DIR:-~/.mason}/mason.sh
 
@@ -12,14 +13,18 @@ function mason_load_source {
         http://downloads.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.bz2 \
         397306fa6d0858c4885fbba7d43a0164dcb7f53e
 
-    mason_extract_tar_bz2
+    mason_extract_tar_bz2 boost_1_57_0/boost
 
-    mkdir -p ${BOOST_ROOT}/include
-    cp -r ${MASON_ROOT}/.build/boost_1_57_0/boost ${BOOST_ROOT}/include/boost
+    MASON_BUILD_PATH=${MASON_ROOT}/.build/boost_1_57_0
 }
 
 function mason_prefix {
     echo "${BOOST_ROOT}"
+}
+
+function mason_compile {
+    mkdir -p ${BOOST_ROOT}/include
+    mv ${MASON_ROOT}/.build/boost_1_57_0/boost ${BOOST_ROOT}/include
 }
 
 function mason_cflags {

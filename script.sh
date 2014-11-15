c3c985ae0b40f5847c11acc36970e9059be8807c
Leith Bade <leith@mapbox.com>
2014-11-15 21:36:06 +1100

    Add support for x86 Android arch

--- script.sh
+++ script.sh
@@ -26,7 +26,11 @@ function mason_prepare_compile {
     elif [ ${MASON_PLATFORM} = 'linux' ]; then
         MASON_OS_COMPILER="linux-x86_64 enable-ec_nistp_64_gcc_128"
     elif [[ ${MASON_PLATFORM} == 'android' ]]; then
-        MASON_OS_COMPILER="android-armv7"
+        if [ ${MASON_ANDROID_ARCH} = 'arm' ]; then
+            MASON_OS_COMPILER="android-armv7"
+        elif [ ${MASON_ANDROID_ARCH} = 'x86' ]; then
+            MASON_OS_COMPILER="android-x86"
+        fi
     fi
 }
 

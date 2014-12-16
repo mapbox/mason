cf49aad9623107f35213149ed66e3020411ca2d2
John Firebaugh <john.firebaugh@gmail.com>
2014-12-15 18:03:47 -0800

    mesa 10.3.5

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 MASON_NAME=mesa
-MASON_VERSION=10.3.3
+MASON_VERSION=10.3.5
 MASON_LIB_FILE=lib/libGL.so
 MASON_PKGCONFIG_FILE=lib/pkgconfig/gl.pc
 
@@ -9,12 +9,12 @@ MASON_PKGCONFIG_FILE=lib/pkgconfig/gl.pc
 
 function mason_load_source {
     mason_download \
-        ftp://ftp.freedesktop.org/pub/mesa/10.3.3/MesaLib-10.3.3.tar.gz \
-        4e17d9e024fc7870a8d516666a40c346beb27c05
+        ftp://ftp.freedesktop.org/pub/mesa/10.3.5/MesaLib-10.3.5.tar.gz \
+        04b9046dfc9aa1558b939aae7032b80e411ff0e0
 
     mason_extract_tar_gz
 
-    export MASON_BUILD_PATH=${MASON_ROOT}/.build/Mesa-10.3.3
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/Mesa-10.3.5
 }
 
 function mason_compile {

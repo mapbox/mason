8f9701d57694b58cf439e01f0af9867cc546351b
Dane Springmeyer <dane@mapbox.com>
2015-01-06 21:15:29 -0800

    update for freetype

--- script.sh
+++ script.sh
@@ -1,41 +1,42 @@
 #!/usr/bin/env bash
 
-MASON_NAME=sqlite
-MASON_VERSION=3.8.6
-MASON_LIB_FILE=lib/libsqlite3.a
-MASON_PKGCONFIG_FILE=lib/pkgconfig/sqlite3.pc
+MASON_NAME=freetype
+MASON_VERSION=2.5.4
+MASON_LIB_FILE=lib/libfreetype.a
+MASON_PKGCONFIG_FILE=lib/pkgconfig/freetype2.pc
 
 . ${MASON_DIR:-~/.mason}/mason.sh
 
 function mason_load_source {
     mason_download \
-        http://www.sqlite.org/2014/sqlite-autoconf-3080600.tar.gz \
-        a97bbc05eeae7a7a6384b3f8c9ff551cf381f041
+        http://nongnu.askapache.com/freetype/freetype-${MASON_VERSION}.tar.bz2 \
+        0646f7e62a6191affe92270e2544e6011f5227e8
 
-    mason_extract_tar_gz
+    mason_extract_tar_bz2
 
-    export MASON_BUILD_PATH=${MASON_ROOT}/.build/sqlite-autoconf-3080600
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/freetype-${MASON_VERSION}
 }
 
 function mason_compile {
-    ./configure \
-        --prefix=${MASON_PREFIX} \
-        ${MASON_HOST_ARG} \
-        --enable-static \
-        --disable-shared \
-        --disable-dependency-tracking
+    ./configure --prefix=${MASON_PREFIX} \
+     --enable-static \
+     --disable-shared ${MASON_HOST_ARG} \
+     --with-zlib=yes \
+     --with-bzip2=no \
+     --with-harfbuzz=no \
+     --with-png=no \
+     --with-quickdraw-toolbox=no \
+     --with-quickdraw-carbon=no \
+     --with-ats=no \
+     --with-fsref=no \
+     --with-fsspec=no \
 
-    make install -j${MASON_CONCURRENCY}
-}
-
-function mason_strip_ldflags {
-    shift # -L...
-    shift # -lsqlite3
-    echo "$@"
+    make -j${MASON_CONCURRENCY}
+    make install
 }
 
 function mason_ldflags {
-    mason_strip_ldflags $(`mason_pkgconfig` --static --libs)
+    echo "-lfreetype -lz"
 }
 
 function mason_clean {

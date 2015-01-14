mapbox/mason

commit 01997a05f6591f41bf838d393c7af3a05ce371a0

Add fetch script to clone/update all branches representing packages - refs #46

mason.sh
@@ -136,6 +136,9 @@ fi
 # Variable defaults
 MASON_HOST_ARG=${MASON_HOST_ARG:-}
 MASON_PLATFORM_VERSION=${MASON_PLATFORM_VERSION:-0}
+MASON_NAME=${MASON_NAME:-nopackage}
+MASON_VERSION=${MASON_VERSION:-noversion}
+MASON_HEADER_ONLY=${MASON_HEADER_ONLY:-false}
 MASON_SLUG=${MASON_NAME}-${MASON_VERSION}
 if [ ${MASON_HEADER_ONLY} ]; then
     MASON_PLATFORM_ID=headers

scripts/fetch.sh
@@ -1,0 +1,21 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+. ${MASON_DIR:-~/.mason}/mason.sh
+
+for b in $(git for-each-ref --sort=-committerdate refs/remotes --format='%(refname:short)'); do
+    NAME=$(basename $b)
+    if [[ ! $NAME =~ "-" ]]; then
+        mason_substep "Skipping branch '$NAME' (does not look like a package as it lacks a dash)"
+        continue
+    fi
+    if [[ ! -d $NAME ]]; then
+        mason_step  "Cloning $NAME"
+        git clone git@github.com:mapbox/mason.git -b $NAME $NAME
+    else
+        mason_step "Pulling and rebasing $NAME"
+        (cd $NAME && git pull --rebase && mason_success "rebased $NAME")
+    fi
+done

commit 950503244a26124fa311c8d3908b980ec74c2564

fix MASON_PLATFORM_ID variable - regressed in 01997a05f6591f41bf8

mason.sh
@@ -140,7 +140,7 @@ MASON_NAME=${MASON_NAME:-nopackage}
 MASON_VERSION=${MASON_VERSION:-noversion}
 MASON_HEADER_ONLY=${MASON_HEADER_ONLY:-false}
 MASON_SLUG=${MASON_NAME}-${MASON_VERSION}
-if [ ${MASON_HEADER_ONLY} ]; then
+if [[ ${MASON_HEADER_ONLY} == true ]]; then
     MASON_PLATFORM_ID=headers
 else
     MASON_PLATFORM_ID=${MASON_PLATFORM}-${MASON_PLATFORM_VERSION}
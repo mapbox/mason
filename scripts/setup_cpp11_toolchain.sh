439aa01bb2bde10c4c21b6880522a175cd28a077
Dane Springmeyer <dane@mapbox.com>
2015-01-28 08:40:47 -0800

    prepare c++11 toolchain script for trusty support

--- scripts/setup_cpp11_toolchain.sh
+++ scripts/setup_cpp11_toolchain.sh
@@ -2,7 +2,7 @@
 function usage() {
     echo "Usage:"
     echo ""
-    echo "source ./scripts/setup_cpp11_toolchain.sh [distro]"
+    echo "source ./scripts/setup_cpp11_toolchain.sh"
     echo ""
     exit 1
 }
@@ -10,41 +10,44 @@ function usage() {
 
 function main() {
 
-    if [[ ${1:-unset} == "unset" ]] || [[ $1 == '-h' ]] || [[ $1 == '--help' ]]; then
+    if [[ $1 == '-h' ]] || [[ $1 == '--help' ]]; then
         usage
     fi
 
-    if [[  ${1} != "precise" ]]; then
-        echo "only precise is supported at this time"
-        exit 1
-    fi
-
-    local distro=$1;
-
-    if [[ ${distro} == "precise" ]]; then
-        release="12.04"
-    else
-        echo "only precise is supported at this time"
-        exit 1
-    fi
-
-    export CPP11_TOOLCHAIN="$(pwd)/toolchain"
-
-    function dpack() {
-        if [[ ! -f $2 ]]; then
-            wget -q $1/$(echo $2 | sed 's/+/%2B/g')
-            dpkg -x $2 ${CPP11_TOOLCHAIN}
-        fi
-    }
-
     if [[ $(uname -s) == 'Linux' ]]; then
+
+        if [[ ! $(lsb_release --id) =~ "Ubuntu" ]]; then
+            echo "only Ubuntu precise is supported at this time and not '$(lsb_release --id)'"
+            exit 1
+        fi
+
+        local codename=$(lsb_release --codename | cut -d : -f 2 | xargs basename)
+        local release=$(lsb_release --release | cut -d : -f 2 | xargs basename)
+
+        export CPP11_TOOLCHAIN="$(pwd)/toolchain"
+
+        function dpack() {
+            if [[ ! -f $2 ]]; then
+                wget -q $1/$(echo $2 | sed 's/+/%2B/g')
+                dpkg -x $2 ${CPP11_TOOLCHAIN}
+            fi
+        }
+
         local PPA="https://launchpad.net/~ubuntu-toolchain-r/+archive/ubuntu/test/+files"
-        # http://llvm.org/apt/precise/dists/llvm-toolchain-${distro}-3.5/main/binary-amd64/Packages
+        # http://llvm.org/apt/precise/dists/llvm-toolchain-${release}-3.5/main/binary-amd64/Packages
         # TODO: cache these for faster downloads
-        local LLVM_DIST="http://llvm.org/apt/${distro}/pool/main/l/llvm-toolchain-3.5"
-        dpack ${LLVM_DIST} clang-3.5_3.5~svn217304-1~exp1_amd64.deb
-        dpack ${LLVM_DIST} libllvm3.5_3.5~svn217304-1~exp1_amd64.deb
-        dpack ${LLVM_DIST} libclang-common-3.5-dev_3.5~svn215019-1~exp1_amd64.deb
+        local LLVM_DIST="http://llvm.org/apt/${release}/pool/main/l/llvm-toolchain-3.5"
+        if [[ $codename == "precise" ]]; then
+            dpack ${LLVM_DIST} clang-3.5_3.5~svn217304-1~exp1_amd64.deb
+            dpack ${LLVM_DIST} libllvm3.5_3.5~svn217304-1~exp1_amd64.deb
+            dpack ${LLVM_DIST} libclang-common-3.5-dev_3.5~svn215019-1~exp1_amd64.deb
+            dpack ${PPA} libstdc++6_4.8.1-2ubuntu1~${release}_amd64.deb
+            dpack ${PPA} libstdc++-4.8-dev_4.8.1-2ubuntu1~${release}_amd64.deb
+            dpack ${PPA} libgcc-4.8-dev_4.8.1-2ubuntu1~${release}_amd64.deb
+        else
+            echo "unsupported distro: $codename $release"
+            exit 1
+        fi
         dpack ${PPA} libstdc++6_4.8.1-2ubuntu1~${release}_amd64.deb
         dpack ${PPA} libstdc++-4.8-dev_4.8.1-2ubuntu1~${release}_amd64.deb
         dpack ${PPA} libgcc-4.8-dev_4.8.1-2ubuntu1~${release}_amd64.deb
@@ -55,7 +58,7 @@ function main() {
         export CXX="${CPP11_TOOLCHAIN}/usr/bin/clang++-3.5"
         export CC="${CPP11_TOOLCHAIN}/usr/bin/clang-3.5"
     else
-        echo "Nothing to be done: this script only bootstraps cpp11 toolchain for linux"
+        echo "Nothing to be done: this script only bootstraps a c++11 toolchain for linux"
     fi
 }
 

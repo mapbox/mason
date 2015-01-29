947fdcc26d1fb09c431d0811f9ecead7d8c85e3c
Dane Springmeyer <dane@mapbox.com>
2015-01-28 19:40:59 -0800

    simplify

--- script.sh
+++ script.sh
@@ -22,12 +22,11 @@ int main() {
     printf(\"%s\", ZLIB_VERSION);
     return 0;
 }
-" > version.c && cc version.c $(mason_cflags) $(mason_ldflags) -o version
+" > version.c && cc version.c -I${MASON_PREFIX}/include/ -L${MASON_PREFIX}/lib -lz -o version
     fi
     ./version
 }
 
-function mason_compile {
 if [[ ${MASON_PLATFORM} = 'osx' || ${MASON_PLATFORM} = 'ios' ]]; then
     MASON_HEADER_FILE="${MASON_SDK_PATH}/usr/include/zlib.h"
     if [ ! -f "${MASON_HEADER_FILE}" ]; then
@@ -40,11 +39,6 @@ if [[ ${MASON_PLATFORM} = 'osx' || ${MASON_PLATFORM} = 'ios' ]]; then
         mason_error "Can't find library file ${MASON_LIBRARY_FILE}"
         exit 1
     fi
-    mkdir -p ${MASON_PREFIX}/lib/
-    mkdir -p ${MASON_PREFIX}/include/
-    ln -s ${MASON_SDK_PATH}/usr/include/zlib.h ${MASON_PREFIX}/include/zlib.h
-    ln -s ${MASON_SDK_PATH}/usr/include/zconf.h ${MASON_PREFIX}/include/zconf.h
-    ln -s ${MASON_SDK_PATH}/usr/lib/libz.dylib ${MASON_PREFIX}/lib/libz.dylib
     MASON_CFLAGS="-I${MASON_PREFIX}/include/"
     MASON_LDFLAGS="-L${MASON_PREFIX}/lib -lz"
 elif [[ ${MASON_PLATFORM} = 'android' ]]; then
@@ -59,24 +53,34 @@ elif [[ ${MASON_PLATFORM} = 'android' ]]; then
         mason_error "Can't find library file ${MASON_LIBRARY_FILE}"
         exit 1
     fi
-
-    ln -s ${MASON_SDK_PATH}/usr/include/zlib.h ${MASON_PREFIX}/include/zlib.h
-    ln -s ${MASON_SDK_PATH}/usr/include/zconf.h ${MASON_PREFIX}/include/zconf.h
-    ln -s ${MASON_SDK_PATH}/usr/lib/libz.dylib ${MASON_PREFIX}/lib/libz.dylib
     MASON_CFLAGS="-I${MASON_PREFIX}/include/"
     MASON_LDFLAGS="-L${MASON_PREFIX}/lib -lz"
 elif [[ -d /usr/include/zlib.h ]] && [[ -d /usr/include/zconf.h ]]; then
-    mkdir -p ${MASON_PREFIX}/lib/
-    mkdir -p ${MASON_PREFIX}/include/
-    ln -s /usr/include/zlib.h ${MASON_PREFIX}/include/zlib.h
-    ln -s /usr/include/zconf.h ${MASON_PREFIX}/include/zconf.h
-    ln -s /usr/lib/libz.so ${MASON_PREFIX}/lib/libz.so
     MASON_CFLAGS="-I${MASON_PREFIX}/include/"
     MASON_LDFLAGS="-L${MASON_PREFIX}/lib -lz"
 else
     MASON_CFLAGS=`pkg-config zlib --cflags`
     MASON_LDFLAGS=`pkg-config zlib --libs`
 fi
+
+function mason_compile {
+    if [[ ${MASON_PLATFORM} = 'osx' || ${MASON_PLATFORM} = 'ios' ]]; then
+        mkdir -p ${MASON_PREFIX}/lib/
+        mkdir -p ${MASON_PREFIX}/include/
+        ln -s ${MASON_SDK_PATH}/usr/include/zlib.h ${MASON_PREFIX}/include/zlib.h
+        ln -s ${MASON_SDK_PATH}/usr/include/zconf.h ${MASON_PREFIX}/include/zconf.h
+        ln -s ${MASON_SDK_PATH}/usr/lib/libz.dylib ${MASON_PREFIX}/lib/libz.dylib
+    elif [[ ${MASON_PLATFORM} = 'android' ]]; then
+        ln -s ${MASON_SDK_PATH}/usr/include/zlib.h ${MASON_PREFIX}/include/zlib.h
+        ln -s ${MASON_SDK_PATH}/usr/include/zconf.h ${MASON_PREFIX}/include/zconf.h
+        ln -s ${MASON_SDK_PATH}/usr/lib/libz.dylib ${MASON_PREFIX}/lib/libz.dylib
+    elif [[ -d /usr/include/zlib.h ]] && [[ -d /usr/include/zconf.h ]]; then
+        mkdir -p ${MASON_PREFIX}/lib/
+        mkdir -p ${MASON_PREFIX}/include/
+        ln -s /usr/include/zlib.h ${MASON_PREFIX}/include/zlib.h
+        ln -s /usr/include/zconf.h ${MASON_PREFIX}/include/zconf.h
+        ln -s /usr/lib/libz.so ${MASON_PREFIX}/lib/libz.so
+    fi
 }
 
 function mason_cflags {

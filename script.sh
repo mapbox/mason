ac60bd1a1486856f4f906c9abffb504b25506309
Dane Springmeyer <dane@mapbox.com>
2015-01-28 21:51:15 -0800

    try to help linux see libpq.a

--- script.sh
+++ script.sh
@@ -48,17 +48,17 @@ function mason_prepare_compile {
 }
 
 function mason_compile {
+    MASON_LIBPQ_PATH=${MASON_LIBPQ}/lib/libpq.a
     CUSTOM_LIBS="-L${MASON_LIBPQ}/lib -L${MASON_TIFF}/lib -ltiff -L${MASON_JPEG}/lib -ljpeg -L${MASON_PROJ}/lib -lproj -L${MASON_PNG}/lib -lpng -L${MASON_EXPAT}/lib -lexpat"
-    CUSTOM_CFLAGS="${CFLAGS} -I${MASON_LIBPQ}/include -I${MASON_TIFF}/include -I${MASON_JPEG}/include -I${MASON_PROJ}/include -lproj -I${MASON_PNG}/include -I${MASON_EXPAT}/include"
+    CUSTOM_CFLAGS="${CFLAGS} -I${MASON_LIBPQ}/include -I${MASON_TIFF}/include -I${MASON_JPEG}/include -I${MASON_PROJ}/include -I${MASON_PNG}/include -I${MASON_EXPAT}/include"
     # note: it might be tempting to build with --without-libtool
     # but I find that will only lead to a static libgdal.a and will
     # not produce a shared library no matter if --enable-shared is passed
 
-    MASON_LIBPQ_PATH=${MASON_LIBPQ}/lib/libpq.a
-    MASON_LIBPQ_PATH=${MASON_LIBPQ_PATH////\\/}
-    perl -i -p -e "s/\-lpq/${MASON_LIBPQ_PATH}/g;" configure
+    MASON_LIBPQ_PATH2=${MASON_LIBPQ_PATH////\\/}
+    perl -i -p -e "s/\-lpq/${MASON_LIBPQ_PATH2}/g;" configure
 
-    LIBS="${CUSTOM_LIBS}" CUSTOM_CFLAGS="${CFLAGS}" ./configure \
+    LDFLAGS="${MASON_LIBPQ_PATH} ${LDFLAGS}" LIBS="${CUSTOM_LIBS}" CUSTOM_CFLAGS="${CFLAGS}" ./configure \
         --enable-static --disable-shared \
         ${MASON_HOST_ARG} \
         --prefix=${MASON_PREFIX} \

e66a905808f56ff2210e75cccbe0851048614c11
Konstantin Käfer <mail@kkaefer.com>
2015-02-02 13:02:56 +0100

    fix system sqlite

--- script.sh
+++ script.sh
@@ -3,27 +3,32 @@
 MASON_NAME=sqlite
 MASON_VERSION=system
 MASON_SYSTEM_PACKAGE=true
+MASON_LIB_FILE=include/sqlite3.h
 
 . ${MASON_DIR:-~/.mason}/mason.sh
 
 if [[ ${MASON_PLATFORM} = 'osx' || ${MASON_PLATFORM} = 'ios' ]]; then
-    MASON_HEADER_FILE="${MASON_SDK_PATH}/usr/include/sqlite3.h"
+    SQLITE_HEADER_DIR="${MASON_SDK_PATH}/usr/include"
+    SQLITE_LIBRARY_DIR="${MASON_SDK_PATH}/usr/lib"
+    MASON_CFLAGS="-I${MASON_PREFIX}/include"
+    MASON_LDFLAGS="-L${MASON_PREFIX}/lib -lsqlite3"
+
+    MASON_HEADER_FILE="${SQLITE_HEADER_DIR}/sqlite3.h"
     if [ ! -f "${MASON_HEADER_FILE}" ]; then
         mason_error "Can't find header file ${MASON_HEADER_FILE}"
         exit 1
     fi
 
-    MASON_LIBRARY_FILE="${MASON_SDK_PATH}/usr/lib/libsqlite3.dylib"
+    MASON_LIBRARY_FILE="${SQLITE_LIBRARY_DIR}/libsqlite3.dylib"
     if [ ! -f "${MASON_LIBRARY_FILE}" ]; then
         mason_error "Can't find library file ${MASON_LIBRARY_FILE}"
         exit 1
     fi
-
-    MASON_CFLAGS=
-    MASON_LDFLAGS=-lsqlite3
 else
-    MASON_CFLAGS=`pkg-config sqlite3 --cflags`
-    MASON_LDFLAGS=`pkg-config sqlite3 --libs`
+    SQLITE_HEADER_DIR="`pkg-config sqlite3 --variable=includedir`"
+    SQLITE_LIBRARY_DIR="`pkg-config sqlite3 --variable=libdir`"
+    MASON_CFLAGS="-I${MASON_PREFIX}/include `pkg-config sqlite3 --cflags-only-other`"
+    MASON_LDFLAGS="-I${MASON_PREFIX}/lib `pkg-config sqlite3 --libs-only-other --libs-only-l`"
 fi
 
 function mason_system_version {
@@ -41,8 +46,10 @@ int main() {
     ./version
 }
 
-function mason_compile {
-    :
+function mason_build {
+    mkdir -p ${MASON_PREFIX}/{include,lib}
+    ln -sf ${SQLITE_HEADER_DIR}/sqlite3.h ${MASON_PREFIX}/include/
+    ln -sf ${SQLITE_LIBRARY_DIR}/libsqlite.* ${MASON_PREFIX}/lib/
 }
 
 function mason_cflags {

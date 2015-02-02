e8d2d2c914315115b76993f42516585b53844e98
Konstantin Käfer <mail@kkaefer.com>
2015-02-02 15:46:25 +0100

    more install fixes

--- script.sh
+++ script.sh
@@ -11,30 +11,31 @@ MASON_CFLAGS="-I${MASON_PREFIX}/include"
 MASON_LDFLAGS="-L${MASON_PREFIX}/lib"
 
 if [[ ${MASON_PLATFORM} = 'osx' || ${MASON_PLATFORM} = 'ios' ]]; then
-    ZLIB_PREFIX="${MASON_SDK_PATH}/usr"
-    ZLIB_LIBRARY="libz.dylib"
+    ZLIB_INCLUDE_PREFIX="${MASON_SDK_PATH}/usr/include"
+    ZLIB_LIBRARY="${MASON_SDK_PATH}/usr/lib/libz.dylib"
     MASON_LDFLAGS="${MASON_LDFLAGS} -lz"
 elif [[ ${MASON_PLATFORM} = 'android' ]]; then
-    ZLIB_PREFIX="${MASON_SDK_PATH}/usr"
-    ZLIB_LIBRARY="libz.so"
+    ZLIB_INCLUDE_PREFIX="${MASON_SDK_PATH}/usr/include"
+    ZLIB_LIBRARY="${MASON_SDK_PATH}/usr/lib/libz.so"
     MASON_LDFLAGS="${MASON_LDFLAGS} -lz"
-elif [[ -d /usr/include/zlib.h ]] && [[ -d /usr/include/zconf.h ]]; then
-    ZLIB_PREFIX="/usr"
-    ZLIB_LIBRARY="libz.so"
+elif [[ -f /usr/include/zlib.h && -f /usr/include/zconf.h ]]; then
+    ZLIB_INCLUDE_PREFIX="/usr/include"
+    ZLIB_LIBRARY="/usr/lib/libz.${MASON_DYNLIB_SUFFIX}"
     MASON_LDFLAGS="${MASON_LDFLAGS} -lz"
 else
-    ZLIB_PREFIX="`pkg-config zlib --variable=prefix`"
-    MASON_CFLAGS="`pkg-config zlib --cflags`"
-    MASON_LDFLAGS="`pkg-config zlib --libs`"
+    ZLIB_INCLUDE_PREFIX="`pkg-config zlib --variable=includedir`"
+    ZLIB_LIBRARY="`pkg-config zlib --variable=libdir`/libz.${MASON_DYNLIB_SUFFIX}"
+    MASON_CFLAGS="${MASON_CFLAGS} `pkg-config zlib --cflags-only-other`"
+    MASON_LDFLAGS="${MASON_LDFLAGS} `pkg-config zlib --libs-only-other --libs-only-l`"
 fi
 
-if [ ! -f "${ZLIB_PREFIX}/include/zlib.h" ]; then
-    mason_error "Can't find header file ${ZLIB_PREFIX}/include/zlib.h"
+if [ ! -f "${ZLIB_INCLUDE_PREFIX}/zlib.h" ]; then
+    mason_error "Can't find header file ${ZLIB_INCLUDE_PREFIX}/zlib.h"
     exit 1
 fi
 
-if [[ ! -z ${ZLIB_LIBRARY} && ! -f "${ZLIB_PREFIX}/lib/${ZLIB_LIBRARY}" ]]; then
-    mason_error "Can't find library file ${ZLIB_PREFIX}/lib/${ZLIB_LIBRARY}"
+if [ ! -f "${ZLIB_LIBRARY}" ]; then
+    mason_error "Can't find library file ${ZLIB_LIBRARY}"
     exit 1
 fi
 
@@ -48,17 +49,15 @@ int main() {
     printf(\"%s\", ZLIB_VERSION);
     return 0;
 }
-" > version.c && ${CC:-cc} version.c $(mason_cflags) -o version
+" > version.c && cc version.c $(mason_cflags) -o version
     fi
     ./version
 }
 
 function mason_build {
     mkdir -p ${MASON_PREFIX}/{include,lib}
-    ln -sf ${ZLIB_PREFIX}/include/{zlib,zconf}.h ${MASON_PREFIX}/include/
-    if [ ! -z ${ZLIB_LIBRARY} ]; then
-        ln -sf ${ZLIB_PREFIX}/lib/${ZLIB_LIBRARY} ${MASON_PREFIX}/lib/
-    fi
+    ln -sf ${ZLIB_INCLUDE_PREFIX}/{zlib,zconf}.h ${MASON_PREFIX}/include/
+    ln -sf ${ZLIB_LIBRARY} ${MASON_PREFIX}/lib/
 }
 
 function mason_cflags {

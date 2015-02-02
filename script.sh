464c51c462e5814b29db4dbb0743f1d02adac807
Konstantin Käfer <mail@kkaefer.com>
2015-02-02 13:05:18 +0100

    some restructuring

--- script.sh
+++ script.sh
@@ -9,33 +9,27 @@ MASON_LIB_FILE=include/curl/curl.h
 
 if [[ ${MASON_PLATFORM} = 'osx' || ${MASON_PLATFORM} = 'ios' ]]; then
     CURL_HEADER_DIR="${MASON_SDK_PATH}/usr/include"
+    CURL_LIBRARY_DIR="${MASON_SDK_PATH}/usr/lib"
+    MASON_CFLAGS="-I${MASON_PREFIX}/include"
+    MASON_LDFLAGS="-L${MASON_PREFIX}/lib -lcurl"
+
     MASON_HEADER_FILE="${CURL_HEADER_DIR}/curl/curl.h"
     if [ ! -f "${MASON_HEADER_FILE}" ]; then
         mason_error "Can't find header file ${MASON_HEADER_FILE}"
         exit 1
     fi
 
-    CURL_LIBRARY_DIR="${MASON_SDK_PATH}/usr/lib"
     MASON_LIBRARY_FILE="${CURL_LIBRARY_DIR}/libcurl.dylib"
     if [ ! -f "${MASON_LIBRARY_FILE}" ]; then
         mason_error "Can't find library file ${MASON_LIBRARY_FILE}"
         exit 1
     fi
-
-    MASON_CFLAGS="-I${MASON_PREFIX}/include"
-    MASON_LDFLAGS="-L${MASON_PREFIX}/lib -lcurl"
 else
     CURL_HEADER_DIR="`pkg-config libcurl --variable=includedir`"
     CURL_LIBRARY_DIR="`pkg-config libcurl --variable=libdir`"
-
     MASON_CFLAGS="-I${MASON_PREFIX}/include `pkg-config libcurl --cflags-only-other`"
     MASON_LDFLAGS="-I${MASON_PREFIX}/lib `pkg-config libcurl --libs-only-other --libs-only-l`"
 fi
-
-
-function mason_load_source {
-    :
-}
 
 function mason_system_version {
     mkdir -p "${MASON_PREFIX}"
@@ -52,7 +46,7 @@ int main() {
     ./version
 }
 
-function mason_compile {
+function mason_build {
     mkdir -p ${MASON_PREFIX}/{include,lib}
     ln -sf ${CURL_HEADER_DIR}/curl ${MASON_PREFIX}/include/
     ln -sf ${CURL_LIBRARY_DIR}/libcurl.* ${MASON_PREFIX}/lib/

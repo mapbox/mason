3cd7d3b69152bf31c15a80ccdd5d4cdb889413d5
Dane Springmeyer <dane@mapbox.com>
2015-02-03 16:23:30 -0800

    resolve symlinks and check arch of shared lib

--- test.sh
+++ test.sh
@@ -30,21 +30,58 @@ function check_ldflags() {
     done
 }
 
+function read_link() {
+    # number of processors on the current system
+    case "$(uname -s)" in
+        'Linux')    readlink -f $1;;
+        'Darwin')   readlink $1;;
+        *)          echo 1;;
+    esac
+}
+
 function check_file_links() {
     if [[ ! -L ./mason_packages/.link/$1 ]]; then
         echo "not ok: ./mason_packages/.link/$1 is not a symlink"
         CODE=1
     else
         echo "ok: ./mason_packages/.link/$1 is a symlink"
+        if [[ ! -f ./mason_packages/.link/$1 ]]; then
+            echo "not ok: ./mason_packages/.link/$1 is not a file"
+            CODE=1
+        else
+            echo "ok: ./mason_packages/.link/$1 is a file"
+        fi
     fi
 
-    if [[ ! -f ./mason_packages/.link/$1 ]]; then
-        echo "not ok: ./mason_packages/.link/$1 is not a file"
-        CODE=1
+}
+
+function check_shared_lib_info() {
+    resolved=$(read_link ./mason_packages/.link/$1)
+    if [[ -f $resolved ]]; then
+        echo "ok: resolved to $resolved"
+        if [[ ${MASON_PLATFORM} == 'osx' ]]; then
+            file $resolved
+            otool -L $resolved
+            lipo -info $resolved
+        elif [[ ${MASON_PLATFORM} == 'ios' ]]; then
+            file $resolved
+            otool -L $resolved
+            lipo -info $resolved
+        elif [[ ${MASON_PLATFORM} == 'linux' ]]; then
+            file $resolved
+            ldd $resolved
+            readelf -d $resolved
+        elif [[ ${MASON_PLATFORM} == 'android' ]]; then
+            file $resolved
+            BIN_PATH=$(~/.mason/mason env MASON_SDK_ROOT)/bin
+            MASON_ANDROID_TOOLCHAIN=$(${MASON_DIR:-~/.mason}/mason env MASON_ANDROID_TOOLCHAIN)
+            ${BIN_PATH}/${MASON_ANDROID_TOOLCHAIN}-readelf -d $resolved
+        fi
+
     else
-        echo "ok: ./mason_packages/.link/$1 is a file"
+        echo "not okay: could not resolve to file: $resolved"
+        CODE=1
     fi
-
 }
 
 check_cflags
@@ -52,6 +89,7 @@ check_ldflags
 check_file_links "include/zlib.h"
 check_file_links "include/zconf.h"
 check_file_links "lib/libz.$(${MASON_DIR:-~/.mason}/mason env MASON_DYNLIB_SUFFIX)"
+check_shared_lib_info "lib/libz.$(${MASON_DIR:-~/.mason}/mason env MASON_DYNLIB_SUFFIX)"
 
 exit ${CODE}
 

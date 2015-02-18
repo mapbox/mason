64263610e4e1c71e8c56784dc8646bb25669a478
Konstantin Käfer <mail@kkaefer.com>
2015-02-17 22:33:28 -0800

    use different package name

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-MASON_NAME=android-ndk-arm-v7
-MASON_VERSION=r10d
+MASON_NAME=android-ndk
+MASON_VERSION=arm-9-r10d
 MASON_LIB_FILE=
 
 . ${MASON_DIR:-~/.mason}/mason.sh
@@ -29,7 +29,7 @@ function mason_compile {
     mkdir -p ${MASON_PREFIX}
 
     ${MASON_BUILD_PATH}/build/tools/make-standalone-toolchain.sh \
-          --toolchain="arm-linux-androideabi-clang3.5" \
+          --toolchain="arm-linux-androideabi-4.9" \
           --llvm-version="3.5" \
           --package-dir="${MASON_BUILD_PATH}/package-dir/" \
           --install-dir="${MASON_PREFIX}" \

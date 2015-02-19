84e7099db740b8704b583c69c7c73fae227890be
Mike Morris <michael.patrick.morris@gmail.com>
2015-02-19 12:25:40 -0500

    https

--- script.sh
+++ script.sh
@@ -9,11 +9,11 @@ MASON_LIB_FILE=bin/node
 function mason_load_source {
     if [ ${MASON_PLATFORM} = 'osx' ]; then
         mason_download \
-            http://nodejs.org/dist/v0.10.35/node-v0.10.35-darwin-x64.tar.gz \
+            https://nodejs.org/dist/v0.10.35/node-v0.10.35-darwin-x64.tar.gz \
             f0311c1291cafe98649e5733210792f7d57cbcd1
     elif [ ${MASON_PLATFORM} = 'linux' ]; then
         mason_download \
-            http://nodejs.org/dist/v0.10.35/node-v0.10.35-linux-x64.tar.gz \
+            https://nodejs.org/dist/v0.10.35/node-v0.10.35-linux-x64.tar.gz \
             ffcb8592e9a2556ea0a25284d5ab5fe608f344c0
     fi
 

a2d4fda4fd08e22b87277e825529afd5e2a2ff81
Mike Morris <michael.patrick.morris@gmail.com>
2015-02-19 12:18:10 -0500

    io.js v1.2.0

--- script.sh
+++ script.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-MASON_NAME=node
-MASON_VERSION=0.12.0
-MASON_LIB_FILE=bin/node
+MASON_NAME=iojs
+MASON_VERSION=1.2.0
+MASON_LIB_FILE=bin/iojs
 
 . ${MASON_DIR:-~/.mason}/mason.sh
 
 function mason_load_source {
     if [ ${MASON_PLATFORM} = 'osx' ]; then
         mason_download \
-            https://nodejs.org/dist/v0.12.0/node-v0.12.0-darwin-x64.tar.gz \
+            https://iojs.org/dist/v1.2.0/iojs-v1.2.0-darwin-x64.tar.gz \
             202d45f836ec45db11a58c4a3ced9774ea2144a2
     elif [ ${MASON_PLATFORM} = 'linux' ]; then
         mason_download \
-            https://nodejs.org/dist/v0.12.0/node-v0.12.0-linux-x64.tar.gz \
+            https://iojs.org/dist/v1.2.0/iojs-v1.2.0-linux-x64.tar.gz \
             350df861e161c34b97398fc1b440f3d80f174cf9
     fi
 

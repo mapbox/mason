db80feec03e7ece001f09bc8105aabb59d9819ba
Mike Morris <michael.patrick.morris@gmail.com>
2015-02-19 12:20:13 -0500

    update hashes

--- script.sh
+++ script.sh
@@ -10,11 +10,11 @@ function mason_load_source {
     if [ ${MASON_PLATFORM} = 'osx' ]; then
         mason_download \
             https://iojs.org/dist/v1.2.0/iojs-v1.2.0-darwin-x64.tar.gz \
-            202d45f836ec45db11a58c4a3ced9774ea2144a2
+            15c553a35abb84085f993e605b83a6b924e22f3c
     elif [ ${MASON_PLATFORM} = 'linux' ]; then
         mason_download \
             https://iojs.org/dist/v1.2.0/iojs-v1.2.0-linux-x64.tar.gz \
-            350df861e161c34b97398fc1b440f3d80f174cf9
+            054234cb47ba4a3b3826a892836760b107596a57
     fi
 
     mason_extract_tar_gz

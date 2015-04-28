c527368af2c7d6c4b9289b05ef6a9ece868bc102
Lauren Budorick <lauren@mapbox.com>
2015-04-28 18:52:47 -0400

    Fix LuaJIT script

--- script.sh
+++ script.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-MASON_NAME=luajit
+MASON_NAME=LuaJIT
 MASON_VERSION=2.0.3
-MASON_LIB_FILE=lib/liblua.a
+MASON_LIB_FILE=lib/libluajit.a
 
 . ${MASON_DIR:-~/.mason}/mason.sh
 
 function mason_load_source {
     mason_download \
         http://luajit.org/download/LuaJIT-2.0.3.tar.gz \
-        3f8d5a84a38423829765512118bbf26c500b0c06
+        0705f5967736b3b01fe7b96af03a8fff45fa43d8
 
     mason_extract_tar_gz
 
@@ -17,7 +17,7 @@ function mason_load_source {
 }
 
 function mason_compile {
-    make generic CC=$CC CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" INSTALL_TOP=${MASON_PREFIX} install
+    make CC=$CC CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" PREFIX=${MASON_PREFIX} install
 }
 
 function mason_cflags {
@@ -25,7 +25,7 @@ function mason_cflags {
 }
 
 function mason_ldflags {
-    echo "-L${MASON_PREFIX}/lib -llua"
+    echo "ld ${MASON_PREFIX}/lib/libluajit-5.1.a"
 }
 
 function mason_clean {

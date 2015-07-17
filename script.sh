bc99d9ea7aeba3b74252a5a8d7feef4ffce7083c
Konstantin Käfer <mail@kkaefer.com>
2015-07-17 12:45:03 +0200

    optionally use a CMake toolchain file

--- script.sh
+++ script.sh
@@ -19,10 +19,17 @@ function mason_load_source {
 function mason_compile {
     mkdir -p build-dir
     cd build-dir
+
+    # patch CMakeLists file
     cat ../CMakeLists.txt | sed -e '/find_package.Sqlite3/ s/^/#/' > ../CMakeLists.txt.new && cp ../CMakeLists.txt.new ../CMakeLists.txt
-    cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=${MASON_PREFIX}
-    make
-    make install
+
+    cmake \
+        -DCMAKE_BUILD_TYPE=RELEASE \
+        -DCMAKE_INSTALL_PREFIX=${MASON_PREFIX} \
+        ${MASON_CMAKE_TOOLCHAIN} \
+        ..
+
+    make install -j${MASON_CONCURRENCY}
 }
 
 function mason_cflags {

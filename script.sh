e82c5ebd02de69dff6bac572a2dc4622dfef7299
John Firebaugh <john.firebaugh@gmail.com>
2015-10-08 11:26:32 -0700

    earcut 0.10

--- script.sh
+++ script.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 MASON_NAME=earcut
-MASON_VERSION=0.9-pool
+MASON_VERSION=0.10
 MASON_HEADER_ONLY=true
 
 . ${MASON_DIR:-~/.mason}/mason.sh
 
 function mason_load_source {
     mason_download \
-    https://github.com/mapbox/earcut.hpp/archive/v0.9-pool.tar.gz \
-    b3dacbd180a5480d797b32ec3bc242b688046e99
+    https://github.com/mapbox/earcut.hpp/archive/v${MASON_VERSION}.tar.gz \
+    a66ce3dc19dbc059d42da52dacb0dd380286555d
     mason_extract_tar_gz
 
-    export MASON_BUILD_PATH=${MASON_ROOT}/.build/earcut.hpp-0.9-pool
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/earcut.hpp-${MASON_VERSION}
 }
 
 function mason_compile {

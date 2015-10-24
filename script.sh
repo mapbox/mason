b3bb81afc3a299c36f6c55a6def2bff77b0969bb
Leith Bade <leith@mapbox.com>
2015-10-24 14:13:26 +1100

    Update to 1.0.2d

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 MASON_NAME=openssl
-MASON_VERSION=1.0.2
+MASON_VERSION=1.0.2d
 MASON_LIB_FILE=lib/libssl.a
 MASON_PKGCONFIG_FILE=lib/pkgconfig/openssl.pc
 
@@ -10,7 +10,7 @@ MASON_PKGCONFIG_FILE=lib/pkgconfig/openssl.pc
 function mason_load_source {
     mason_download \
         ftp://ftp.openssl.org/source/openssl-${MASON_VERSION}.tar.gz \
-        edb5a59838b24d0845950feed47dae014c66b2ab
+        ec5b1937e8dcbf8c1831bf67f3744474f0f04bf8
 
     mason_extract_tar_gz
 

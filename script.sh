ed870c80fcc280fe526b81db035f64abec004d9f
John Firebaugh <john.firebaugh@gmail.com>
2016-03-03 17:37:43 -0800

    jni.hpp v2.0.0

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 MASON_NAME=jni.hpp
-MASON_VERSION=1.0.0
+MASON_VERSION=2.0.0
 MASON_HEADER_ONLY=true
 
 . ${MASON_DIR:-~/.mason}/mason.sh
@@ -9,7 +9,7 @@ MASON_HEADER_ONLY=true
 function mason_load_source {
     mason_download \
     https://github.com/mapbox/jni.hpp/archive/v${MASON_VERSION}.tar.gz \
-    eff354ffb0c01d1bcca562c779d78d4261516eeb
+    02a99b3a4b55686a8956c8bef9de036819826de0
     mason_extract_tar_gz
 
     export MASON_BUILD_PATH=${MASON_ROOT}/.build/jni.hpp-${MASON_VERSION}

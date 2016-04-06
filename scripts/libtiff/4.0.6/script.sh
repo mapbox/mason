48d2cc953005a20e3ac1c11ee7e73f9c6d3dadaf
Dane Springmeyer <dane@mapbox.com>
2016-04-06 14:59:53 -0400

    use latest jpeg turbo [skip ci]

--- scripts/libtiff/4.0.6/script.sh
+++ scripts/libtiff/4.0.6/script.sh
@@ -19,8 +19,8 @@ function mason_load_source {
 
 function mason_prepare_compile {
     cd $(dirname ${MASON_ROOT})
-    ${MASON_DIR:-~/.mason}/mason install jpeg_turbo 1.4.0
-    MASON_JPEG=$(${MASON_DIR:-~/.mason}/mason prefix jpeg_turbo 1.4.0)
+    ${MASON_DIR:-~/.mason}/mason install jpeg_turbo 1.4.2
+    MASON_JPEG=$(${MASON_DIR:-~/.mason}/mason prefix jpeg_turbo 1.4.2)
     SYSTEM_ZLIB="/usr"
 }
 

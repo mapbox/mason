ab45a54aaa4d9d68b7bb936ec729470a26564338
Bruno de Oliveira Abinader <bruno@mapbox.com>
2016-06-14 14:41:26 +0300

    Include clang standard C++ headers in clang-tidy

--- scripts/clang-tidy/3.8.0/script.sh
+++ scripts/clang-tidy/3.8.0/script.sh
@@ -15,7 +15,11 @@ function mason_build {
     ${MASON_DIR}/mason install clang ${MASON_VERSION}
     CLANG_PREFIX=$(${MASON_DIR}/mason prefix clang ${MASON_VERSION})
     mkdir -p "${MASON_PREFIX}/bin"
+    mkdir -p "${MASON_PREFIX}/include/c++"
+    mkdir -p "${MASON_PREFIX}/lib/clang/${MASON_VERSION}/include"
     cp ${CLANG_PREFIX}/bin/${MASON_NAME} "${MASON_PREFIX}/bin/"
+    cp -R ${CLANG_PREFIX}/include/c++ "${MASON_PREFIX}/include/c++"
+    cp -R ${CLANG_PREFIX}/lib/clang/${MASON_VERSION}/include "${MASON_PREFIX}/lib/clang/${MASON_VERSION}/include"
 }
 
 mason_run "$@"

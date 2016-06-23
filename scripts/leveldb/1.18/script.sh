dc54bbca5ba5e72184f2aa0cbc57bb576f9ac2dc
Dane Springmeyer <dane@mapbox.com>
2016-06-23 10:29:42 -0700

    [leveldb] finish building out install target

--- scripts/leveldb/1.18/script.sh
+++ scripts/leveldb/1.18/script.sh
@@ -4,7 +4,7 @@ MASON_NAME=leveldb
 MASON_VERSION=1.18
 MASON_LIB_FILE=lib/libleveldb.a
 
-. ${MASON_DIR:-~/.mason}/mason.sh
+. ${MASON_DIR}/mason.sh
 
 function mason_load_source {
     mason_download \
@@ -13,18 +13,25 @@ function mason_load_source {
 
     mason_extract_tar_gz
 
-    export MASON_BUILD_PATH=${MASON_ROOT}/.build/leveldb-${MASON_VERSION} 
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/${MASON_NAME}-${MASON_VERSION}
 }
 
 function mason_compile {
+    # note: OPT is set in the Makefile and defaults to '-O2 -DNDEBUG'
+    # (dane) we should use -O3 for the fastest code - I presume -O2
+    # is being used as safe default since some old compilers were buggy
+    # with -O3 back in the day...
+    OPT="-O3 -DNDEBUG" make libleveldb.a -j${MASON_CONCURRENCY}
+    # leveldb lacks an install target
+    # https://github.com/google/leveldb/pull/2
     mkdir -p ${MASON_PREFIX}/include/
+    mkdir -p ${MASON_PREFIX}/lib/
     cp -r include/leveldb ${MASON_PREFIX}/include/leveldb
-
-    make -j${MASON_CONCURRENCY}
+    cp libleveldb.a ${MASON_PREFIX}/lib/
 }
 
 function mason_ldflags {
-    echo -L${MASON_PREFIX}/lib -lleveldb -lsnappy
+    echo -L${MASON_PREFIX}/lib -lleveldb
 }
 
 function mason_clean {

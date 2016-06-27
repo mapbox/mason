fa162973796720346b862d6e22dd1a02bbda22c5
mapsam <matthews.sam@gmail.com>
2016-06-27 11:15:00 -0700

    update repo name

--- scripts/hpp_skel/0.0.1/script.sh
+++ scripts/hpp_skel/0.0.1/script.sh
@@ -8,7 +8,7 @@ MASON_HEADER_ONLY=true
 
 function mason_load_source {
     mason_download \
-        https://github.com/mapbox/${MASON_NAME}/archive/v${MASON_VERSION}.tar.gz \
+        https://github.com/mapbox/hpp-skel/archive/v${MASON_VERSION}.tar.gz \
         aca66253eb73fdac4433c0207c508ad155b2f8de
 
     mason_extract_tar_gz

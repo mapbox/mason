4683a8d13398cd7ab2be35fb6db679caedfb1a95
Konstantin Käfer <mail@kkaefer.com>
2016-07-26 15:43:14 +0200

    remove pkg-config usage from libpng

--- scripts/libpng/1.6.20/script.sh
+++ scripts/libpng/1.6.20/script.sh
@@ -9,7 +9,7 @@ MASON_PKGCONFIG_FILE=lib/pkgconfig/libpng.pc
 
 function mason_load_source {
     mason_download \
-        http://downloads.sourceforge.net/project/libpng/libpng16/1.6.20/libpng-${MASON_VERSION}.tar.gz \
+        http://downloads.sourceforge.net/project/libpng/libpng16/older-releases/${MASON_VERSION}/libpng-${MASON_VERSION}.tar.gz \
         0b5df1201ea4b63777a9c9c49ff26a45dd87890e
 
     mason_extract_tar_gz
@@ -37,7 +37,17 @@ function mason_strip_ldflags {
 }
 
 function mason_ldflags {
-    mason_strip_ldflags $(`mason_pkgconfig` --static --libs)
+    local PREFIX=$(${MASON_PREFIX}/bin/libpng-config --prefix)
+    local FLAGS=$(${MASON_PREFIX}/bin/libpng-config --static --ldflags)
+    # Replace hard-coded prefix with the current one.
+    mason_strip_ldflags ${FLAGS//${PREFIX}/${MASON_PREFIX}}
+}
+
+function mason_cflags {
+    local PREFIX=$(${MASON_PREFIX}/bin/libpng-config --prefix)
+    local FLAGS=$(${MASON_PREFIX}/bin/libpng-config --static --cflags)
+    # Replace hard-coded prefix with the current one.
+    echo ${FLAGS//${PREFIX}/${MASON_PREFIX}}
 }
 
 function mason_clean {

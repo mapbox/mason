14876384aa55ee424c640f1618360434061139c8
mapsam <matthews.sam@gmail.com>
2016-08-11 07:52:43 -0500

    update hash

--- scripts/vectortile/1.0.0-rc1/script.sh
+++ scripts/vectortile/1.0.0-rc1/script.sh
@@ -9,11 +9,11 @@ MASON_HEADER_ONLY=true
 function mason_load_source {
     mason_download \
         https://github.com/mapbox/vector-tile/archive/v${MASON_VERSION}.tar.gz \
-        2876991412fcfd41bd7d606e78025e2d3f6e319b
+        cc9b976c7a885702b6cb07370d99f0751a47f6f7
 
     mason_extract_tar_gz
 
-    export MASON_BUILD_PATH=${MASON_ROOT}/.build/${MASON_NAME}-${MASON_VERSION}
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/vector-tile-${MASON_VERSION}
 }
 
 function mason_compile {

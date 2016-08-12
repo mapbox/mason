14f17ef8f68ebdea74d9f8a297b1e859940f3acb
Dane Springmeyer <dane@mapbox.com>
2016-08-12 16:43:52 -0700

    upgrade jpeg used by libtiff

--- scripts/libtiff/4.0.6/script.sh
+++ scripts/libtiff/4.0.6/script.sh
@@ -19,8 +19,8 @@ function mason_load_source {
 
 function mason_prepare_compile {
     cd $(dirname ${MASON_ROOT})
-    ${MASON_DIR}/mason install jpeg_turbo 1.4.2
-    MASON_JPEG=$(${MASON_DIR}/mason prefix jpeg_turbo 1.4.2)
+    ${MASON_DIR}/mason install jpeg_turbo 1.5.0
+    MASON_JPEG=$(${MASON_DIR}/mason prefix jpeg_turbo 1.5.0)
     SYSTEM_ZLIB="/usr"
 }
 

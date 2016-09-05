2b550ea353f2cbfac823bdd6224688f1f3c35a3f
Dane Springmeyer <dane@mapbox.com>
2016-09-04 18:04:18 -0700

    Strip unused code

--- scripts/clang/4.x/script.sh
+++ scripts/clang/4.x/script.sh
@@ -6,18 +6,8 @@ MASON_LIB_FILE=bin/clang
 
 . ${MASON_DIR}/mason.sh
 
-
 # options
 ENABLE_LLDB=false
-
-function curl_get() {
-    if [ ! -f $(basename ${1}) ] ; then
-        mason_step "Downloading $1 to $(pwd)/$(basename ${1})"
-        curl --retry 3 -f -L -O "$1"
-    else
-        echo "already downloaded $1 to $(pwd)/$(basename ${1})"
-    fi
-}
 
 function git_get() {
     if [ ! -d ${2} ] ; then
@@ -45,42 +35,6 @@ function setup() {
     git_get http://llvm.org/git/compiler-rt.git ${LLVM_RELEASE}/llvm/projects/compiler-rt
     cp -r ${LLVM_RELEASE}/llvm/* ${BUILD_PATH}/
 }
-
-function setup_release() {
-    LLVM_RELEASE=$1
-    BUILD_PATH=$2
-    curl_get "http://llvm.org/releases/${LLVM_RELEASE}/llvm-${LLVM_RELEASE}.src.tar.xz"
-    curl_get "http://llvm.org/releases/${LLVM_RELEASE}/cfe-${LLVM_RELEASE}.src.tar.xz"
-    curl_get "http://llvm.org/releases/${LLVM_RELEASE}/compiler-rt-${LLVM_RELEASE}.src.tar.xz"
-    curl_get "http://llvm.org/releases/${LLVM_RELEASE}/libcxx-${LLVM_RELEASE}.src.tar.xz"
-    curl_get "http://llvm.org/releases/${LLVM_RELEASE}/libcxxabi-${LLVM_RELEASE}.src.tar.xz"
-    curl_get "http://llvm.org/releases/${LLVM_RELEASE}/libunwind-${LLVM_RELEASE}.src.tar.xz"
-    curl_get "http://llvm.org/releases/${LLVM_RELEASE}/lld-${LLVM_RELEASE}.src.tar.xz"
-    if [[ ${ENABLE_LLDB} == true ]]; then
-        curl_get "http://llvm.org/releases/${LLVM_RELEASE}/lldb-${LLVM_RELEASE}.src.tar.xz"
-    fi
-    #curl_get "http://llvm.org/releases/${LLVM_RELEASE}/openmp-${LLVM_RELEASE}.src.tar.xz"
-    curl_get "http://llvm.org/releases/${LLVM_RELEASE}/clang-tools-extra-${LLVM_RELEASE}.src.tar.xz"
-    for i in $(ls *.xz); do
-        echo "unpacking $i"
-        tar xf $i;
-    done
-    mv llvm-${LLVM_RELEASE}.src/* ${BUILD_PATH}/
-    ls ${BUILD_PATH}/
-    mv cfe-${LLVM_RELEASE}.src ${BUILD_PATH}/tools/clang
-    mv compiler-rt-${LLVM_RELEASE}.src ${BUILD_PATH}/projects/compiler-rt
-    mv libcxx-${LLVM_RELEASE}.src ${BUILD_PATH}/projects/libcxx
-    mv libcxxabi-${LLVM_RELEASE}.src ${BUILD_PATH}/projects/libcxxabi
-    mv libunwind-${LLVM_RELEASE}.src ${BUILD_PATH}/projects/libunwind
-    mv lld-${LLVM_RELEASE}.src ${BUILD_PATH}/tools/lld
-    if [[ ${ENABLE_LLDB} == true ]]; then
-        mv lldb-${LLVM_RELEASE}.src ${BUILD_PATH}/tools/lldb
-    fi
-    #mv openmp-${LLVM_RELEASE}.src ${BUILD_PATH}/projects/openmp
-    mv clang-tools-extra-${LLVM_RELEASE}.src ${BUILD_PATH}/tools/clang/tools/extra
-    cd ../
-}
-
 
 function mason_load_source {
     mkdir -p "${MASON_ROOT}/.cache"

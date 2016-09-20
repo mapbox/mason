5d00bf0c80eb3102fefe0da0dc60e80854418d65
Dane Springmeyer <dane@mapbox.com>
2016-09-20 11:20:52 -0700

    solve broken symlinks for bzip2 - closes #80

--- scripts/bzip2/1.0.6/script.sh
+++ scripts/bzip2/1.0.6/script.sh
@@ -16,8 +16,24 @@ function mason_load_source {
     export MASON_BUILD_PATH=${MASON_ROOT}/.build/${MASON_NAME}-${MASON_VERSION}
 }
 
+function read_link() {
+    case "$(uname -s)" in
+        'Linux')    readlink -f $1;;
+        'Darwin')   readlink $1;;
+        *)          echo 1;;
+    esac
+}
+
 function mason_compile {
     make install PREFIX=${MASON_PREFIX} CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
+    # symlinks are not portable, so now we recurse into /bin directory
+    # and fix them to be portable by being relative
+    cd ${MASON_PREFIX}/bin
+    for i in $(ls *); do
+        if [[ -L $i ]]; then
+            ln -sf $(basename $(read_link $i)) $i
+        fi
+    done
     # TODO: android may need ranlib manual call
 }
 

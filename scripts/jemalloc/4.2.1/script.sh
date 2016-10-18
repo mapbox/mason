c1e4d0b97a1bad553178bc58392bc796f26aeac4
Dane Springmeyer <dane@mapbox.com>
2016-10-18 12:47:25 -0700

    jemalloc build fixes [skip ci]

--- scripts/jemalloc/4.2.1/script.sh
+++ scripts/jemalloc/4.2.1/script.sh
@@ -11,13 +11,14 @@ function mason_load_source {
         https://github.com/jemalloc/jemalloc/releases/download/${MASON_VERSION}/jemalloc-${MASON_VERSION}.tar.bz2 \
         6bd515d75d192ac82f3171d36a998e0e6e77ac8a
 
-    mason_extract_tar_gz
+    mason_extract_tar_bz2
 
     export MASON_BUILD_PATH=${MASON_ROOT}/.build/${MASON_NAME}-${MASON_VERSION}
 }
 
 function mason_compile {
-    unset CFLAGS
+    # warning: CFLAGS overwrites jemalloc CFLAGS, so we need to add back the jemalloc defaults
+    export CFLAGS="${CFLAGS} -std=gnu99 -Wall -pipe -O3 -funroll-loops"
     ./configure --prefix=${MASON_PREFIX}
     make -j${MASON_CONCURRENCY} VERBOSE=1
     make install

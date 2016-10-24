70a55e88128040a9b91813114ddda026bbb267cb
Konstantin Käfer <mail@kkaefer.com>
2016-10-24 12:33:30 -0700

    gtest 1.7.0 [skip ci]

--- scripts/gtest/1.7.0/script.sh
+++ scripts/gtest/1.7.0/script.sh
@@ -10,37 +10,58 @@ MASON_LIB_FILE=lib/libgtest.a
 
 function mason_load_source {
     mason_download \
-        https://googletest.googlecode.com/files/gtest-${LIB_VERSION}.zip \
-        86f5fd0ce20ef1283092d1d5a4bc004916521aaf
+        https://github.com/google/googletest/archive/release-${LIB_VERSION}.tar.gz \
+        88afbaea71b4ba50b25f52e13e8c3b01f0f99494
 
+    mason_extract_tar_gz
 
-    mason_setup_build_dir
-    unzip ../.cache/${MASON_SLUG}
-
-    export MASON_BUILD_PATH=${MASON_ROOT}/.build/gtest-${LIB_VERSION}
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/googletest-release-${LIB_VERSION}
 }
 
-function mason_compile {
-    if [ ${MASON_PLATFORM} = 'ios' ]; then
-        ${CXX:-c++} ${CFLAGS:-} -O3 -g -isystem fused-src -pthread -c -fPIC fused-src/gtest/gtest-all.cc
-        mkdir -p lib/.libs
-        libtool -static gtest-all.o -o lib/.libs/libgtest.a
-    else
-        ./configure \
-            --prefix=${MASON_PREFIX} \
-            ${MASON_HOST_ARG} \
-            --enable-static \
-            --with-pic \
-            --disable-shared \
-            --disable-dependency-tracking
-
-        make -j${MASON_CONCURRENCY}
-    fi
+function mason_build {
+    mason_load_source
+    mason_step "Building for Platform '${MASON_PLATFORM}/${MASON_PLATFORM_VERSION}'..."
+    cd "${MASON_BUILD_PATH}"
 
     mkdir -p ${MASON_PREFIX}/lib
-    cp -v lib/.libs/libgtest.a ${MASON_PREFIX}/lib
     mkdir -p ${MASON_PREFIX}/include/gtest
-    cp -v fused-src/gtest/gtest.h ${MASON_PREFIX}/include/gtest
+    cp -v include/gtest/gtest.h ${MASON_PREFIX}/include/gtest
+
+    rm -rf build
+    mkdir -p build
+    cd build
+    if [ ${MASON_PLATFORM} = 'ios' ]; then
+        cmake \
+            -GXcode \
+            -DCMAKE_TOOLCHAIN_FILE=${MASON_DIR}/utils/ios.cmake \
+            ..
+        xcodebuild -configuration Release -sdk iphoneos
+        xcodebuild -configuration Release -sdk iphonesimulator
+
+        mason_substep "Creating Universal Binary..."
+        LIB_FOLDERS="Release-iphoneos Release-iphonesimulator"
+        mkdir -p ${MASON_PREFIX}/lib
+        for LIB in $(find ${LIB_FOLDERS} -name "*.a" | xargs basename | sort | uniq) ; do
+            lipo -create $(find ${LIB_FOLDERS} -name "${LIB}") -output ${MASON_PREFIX}/lib/${LIB}
+            lipo -info ${MASON_PREFIX}/lib/${LIB}
+        done
+    elif [ ${MASON_PLATFORM} = 'android' ]; then
+        ${MASON_DIR}/utils/android.sh > toolchain.cmake
+        cmake \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_INSTALL_PREFIX=${MASON_PREFIX} \
+            -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake \
+            ..
+        make VERBOSE=1 -j${MASON_CONCURRENCY} gtest
+        cp -v libgtest.a ${MASON_PREFIX}/lib
+    else
+        cmake \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_INSTALL_PREFIX=${MASON_PREFIX} \
+            ..
+        make VERBOSE=1 -j${MASON_CONCURRENCY} gtest
+        cp -v libgtest.a ${MASON_PREFIX}/lib
+    fi
 }
 
 function mason_cflags {

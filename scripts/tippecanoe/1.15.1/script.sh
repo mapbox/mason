91cc1ce46898139833bb7cfa4609bd2cead1816a
Dane Springmeyer <dane@mapbox.com>
2016-12-06 16:40:17 -0800

    adjustments to tippecanoe build

--- scripts/tippecanoe/1.15.1/script.sh
+++ scripts/tippecanoe/1.15.1/script.sh
@@ -8,32 +8,41 @@ MASON_LIB_FILE=bin/tippecanoe
 
 function mason_load_source {
     mason_download \
-        https://github.com/mapbox/tippecanoe/tarball/1.15.1 \
-        1ff0482c1b60b9204d5748d911a6a8aa419b390e
+        https://github.com/mapbox/tippecanoe/archive/${MASON_VERSION}.tar.gz \
+        0b31a40e7ababf34fa5945afc538224d86f6b729
 
     mason_extract_tar_gz
 
-    export MASON_BUILD_PATH=${MASON_ROOT}/.build/mapbox-tippecanoe-c68d553
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/${MASON_NAME}-${MASON_VERSION}
 }
 
 function mason_prepare_compile {
-    cd $(dirname ${MASON_ROOT})
-    ${MASON_DIR}/mason install protobuf 2.6.1
-    ${MASON_DIR}/mason link protobuf 2.6.1
-    ${MASON_DIR}/mason install sqlite 3.9.1
-    ${MASON_DIR}/mason link sqlite 3.9.1
+    ${MASON_DIR}/mason install sqlite 3.14.2
+    MASON_SQLITE=$(${MASON_DIR}/mason prefix sqlite 3.14.2)
 }
 
 function mason_compile {
     PREFIX=${MASON_PREFIX} \
-    PATH=${MASON_DIR}/mason_packages/.link/bin:${PATH} \
-    CXXFLAGS=-I${MASON_DIR}/mason_packages/.link/include \
-    LDFLAGS="-L${MASON_DIR}/mason_packages/.link/lib -lprotobuf-lite -ldl -lpthread" make
-    
+    PATH=${MASON_SQLITE}/bin:${PATH} \
+    CXXFLAGS="${CXXFLAGS} -I${MASON_SQLITE}/include" \
+    LDFLAGS="${LDFLAGS} -L${MASON_SQLITE}/lib -ldl -lpthread" make
+
     PREFIX=${MASON_PREFIX} \
-    PATH=${MASON_DIR}/mason_packages/.link/bin:${PATH} \
-    CXXFLAGS=-I${MASON_DIR}/mason_packages/.link/include \
-    LDFLAGS="-L${MASON_DIR}/mason_packages/.link/lib -lprotobuf-lite -ldl -lpthread" make install
+    PATH=${MASON_SQLITE}/bin:${PATH} \
+    CXXFLAGS="${CXXFLAGS} -I${MASON_SQLITE}/include" \
+    LDFLAGS="${LDFLAGS} -L${MASON_SQLITE}/lib -ldl -lpthread" make install
+}
+
+function mason_cflags {
+    :
+}
+
+function mason_ldflags {
+    :
+}
+
+function mason_static_libs {
+    :
 }
 
 function mason_clean {

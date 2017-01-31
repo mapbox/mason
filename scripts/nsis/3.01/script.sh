a8755a441f80b95daf6550d20d3c6ec002391b86
Dane Springmeyer <dane@mapbox.com>
2017-01-30 20:54:23 -0800

    ensure all the nsis data is packaged [skip ci]

--- scripts/nsis/3.01/script.sh
+++ scripts/nsis/3.01/script.sh
@@ -17,8 +17,10 @@ function mason_load_source {
 }
 
 function mason_compile {
-    wget http://prdownloads.sourceforge.net/scons/scons-local-2.5.1.tar.gz
-    tar xvf scons-local-2.5.1.tar.gz
+    if [ ! -f scons-local-2.5.1.tar.gz ]; then
+        wget http://prdownloads.sourceforge.net/scons/scons-local-2.5.1.tar.gz
+        tar xvf scons-local-2.5.1.tar.gz
+    fi
     perl -i -p -e "s/'__attribute__\(\(__stdcall__\)\)'/'\"__attribute__\(\(__stdcall__\)\)\"'/g" SCons/Config/gnu
     if [[ $(uname -s) == 'Darwin' ]]; then
         perl -i -p -e "s/'-Wall'/'-Wall','-stdlib=libstdc++','-fpermissive'/g" SCons/Config/gnu
@@ -27,16 +29,27 @@ function mason_compile {
         perl -i -p -e "s/'-Wall'/'-Wall','-fpermissive'/g" SCons/Config/gnu
         perl -i -p -e "s/'-m32'/'-m64'/g" SCons/Config/gnu
     fi
-    python scons.py STRIP=0 SKIPUTILS=all PREFIX=/tmp/makensis-data makensis
+    #perl -i -p -e "s/#include <shlobj.h>//g" Source/exehead/util.h
+    python scons.py STRIP=0 SKIPUTILS=all PREFIX=${MASON_PREFIX} makensis
     mkdir -p ${MASON_PREFIX}/bin
     cp build/urelease/makensis/makensis ${MASON_PREFIX}/bin/
+    mkdir -p ${MASON_PREFIX}/Include
+    if [ ! -f nsis-${MASON_VERSION}.zip ]; then
+        wget https://downloads.sourceforge.net/project/nsis/NSIS%203/${MASON_VERSION}/nsis-${MASON_VERSION}.zip
+        unzip nsis-${MASON_VERSION}.zip
+    fi
+    mkdir -p ${MASON_PREFIX}/Plugins/
+    cp -r nsis-${MASON_VERSION}/Plugins/* ${MASON_PREFIX}/Plugins/ 
+    mkdir -p ${MASON_PREFIX}/Include/
+    cp -r nsis-${MASON_VERSION}/Include/* ${MASON_PREFIX}/Include/ 
+    mkdir -p ${MASON_PREFIX}/share/nsis/Contrib/
+    mkdir -p ${MASON_PREFIX}/Contrib
+    cp -r nsis-${MASON_VERSION}/Contrib/* ${MASON_PREFIX}/share/nsis/Contrib/
+    cp -r nsis-${MASON_VERSION}/Contrib/* ${MASON_PREFIX}/Contrib/
     mkdir -p ${MASON_PREFIX}/share/nsis/Stubs/
-    wget https://downloads.sourceforge.net/project/nsis/NSIS%203/${MASON_VERSION}/nsis-${MASON_VERSION}.zip
-    unzip nsis-${MASON_VERSION}.zip
+    mkdir -p ${MASON_PREFIX}/Stubs/
+    cp nsis-${MASON_VERSION}/Stubs/* ${MASON_PREFIX}/Stubs/
     cp nsis-${MASON_VERSION}/Stubs/* ${MASON_PREFIX}/share/nsis/Stubs/
-    # note: upon install this needs to be copied in place:
-    # mkdir -p /tmp/makensis-data/share/nsis/Stubs/
-    # cp -r $(./mason prefix nsis 3.01)/share/nsis/Stubs/* /tmp/makensis-data/share/nsis/Stubs/
 }
 
 function mason_cflags {

073d829ff768483ce6a33cb014d61b1264ceabf3
Dane Springmeyer <dane@mapbox.com>
2017-02-22 09:10:38 -0800

    minor adjustments to libzmq package
     - Mason sets CFLAGS/CXXFLAGS which means we need to set the optimization flag -O3 manually in the script
     - No need to run autogen.sh (configure works just fine)
     - Reuse the version variable in a few places (will make it easier to copy/paste this script for use with new versions)
     - Patch the .pc file to avoid libstdc++ being reported in use on OS X (since it is not)
     - Separate make from make install per convention (avoids partial installs on build failure)

--- scripts/libzmq/4.2.2/script.sh
+++ scripts/libzmq/4.2.2/script.sh
@@ -9,7 +9,7 @@ MASON_PKGCONFIG_FILE=lib/pkgconfig/libzmq.pc
 
 function mason_load_source {
     mason_download \
-        https://github.com/zeromq/libzmq/releases/download/v4.2.2/zeromq-4.2.2.tar.gz \
+        https://github.com/zeromq/libzmq/releases/download/v${MASON_VERSION}/zeromq-${MASON_VERSION}.tar.gz \
         3ff55a1c2b23ad1a586789747c9837bd0729bb6d
 
     mason_extract_tar_gz
@@ -18,6 +18,14 @@ function mason_load_source {
 }
 
 function mason_compile {
+    # note: mason CFLAGS/CXXFLAGS override default (-g -O2)
+    # so we add optmization settings now
+    export CFLAGS="${CFLAGS} -O3 -DNDEBUG"
+    export CXXFLAGS="${CXXFLAGS} -O3 -DNDEBUG"
+
+    # we link to libc++ on OS X so we need to knock out the libstdc++ assumption in the .pc file
+    perl -i -p -e "s/-lstdc\+\+//g;" libzmq.pc.in
+
     ./configure \
         --prefix=${MASON_PREFIX} \
         ${MASON_HOST_ARG} \
@@ -25,9 +33,8 @@ function mason_compile {
         --disable-shared \
         --disable-dependency-tracking
 
-    ./autogen.sh
-
-    make install -j${MASON_CONCURRENCY}
+    V=1 make -j${MASON_CONCURRENCY}
+    make install
 }
 
 function mason_clean {

1c53bd00b062ce6e9cbd1f2b5fa3928153b28d93
Dane Springmeyer <dane@mapbox.com>
2017-03-06 12:47:21 -0800

    fix broken expat 2.1.1 download link

--- test/c_build.sh
+++ test/c_build.sh
@@ -3,8 +3,8 @@
 set -e -u
 set -o pipefail
 
-./mason build expat 2.0.1
-./mason build expat 2.0.2
+./mason build expat 2.1.1
+./mason build expat 2.2.0
 ./mason build libzip 1.1.3
 ./mason build zlib 1.2.8
 ./mason build libuv 0.11.29

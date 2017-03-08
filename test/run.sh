ba8145a59b51e31fa6a9e21f28d726c335b6d7e1
Konstantin Käfer <mail@kkaefer.com>
2017-03-07 17:59:41 -0800

    remove duplicated test function

--- test/run.sh
+++ test/run.sh
@@ -21,7 +21,7 @@ function finish {
 }
 trap finish EXIT
 
-function mason_error {
+function mason_error_handler {
     local _LINE _FN _FILE
     read _LINE _FN _FILE <<< "`caller 0`"
     >&2 echo -e "\033[1m\033[31mFailure in ${_FILE} on line ${_LINE}\033[0m"
@@ -49,31 +49,8 @@ function mason_succeeds {
     fi
 }
 
-function mason_fails {
-    >&2 echo -n "- Testing '$1'"
-    shift
-    local OUTPUT
-    if OUTPUT=$("$@" 2>&1); then
-        local LINE FN FILE
-        read LINE FN FILE <<< "`caller 0`"
-        >&2 echo -e " \033[1m\033[31mSUCCEEDED erroneously\033[0m in ${FILE} on line ${LINE}"
-        if [ ! -z "$@" ]; then
-            >&2 echo -ne "  \033[1mEvaluated\033[0m: "
-            >&2 sed '2,$s/^/             /' <<< "$@"
-        fi
-        if [ ! -z "${OUTPUT}" ]; then
-            >&2 echo -ne "     \033[1mOutput\033[0m: "
-            >&2 sed '2,$s/^/             /' <<< "${OUTPUT}"
-        fi
-        return 1
-    else
-        >&2 echo -e " \033[1m\033[32mfails expectedly\033[0m"
-    fi
-}
-
-export mason_error
+export mason_error_handler
 export mason_succeeds
-export mason_fails
 
 FAILED=false
 
@@ -81,7 +58,7 @@ function run_test {
     rm -rf mason_packages
     local OUTPUT
     >&2 echo -en "* Running \033[1m$1\033[0m"
-    if ! OUTPUT=$(set -euo pipefail; trap mason_error ERR; source "$1" 2>&1); then
+    if ! OUTPUT=$(set -euo pipefail; trap mason_error_handler ERR; source "$1" 2>&1); then
         >&2 echo -e " \033[1m\033[31mFAILED\033[0m:"
         FAILED=true
     else
--- test/test_package_install_failure.sh
+++ test/test_package_install_failure.sh
@@ -1,4 +1,10 @@
-mason_fails "install an invalid version" ./mason.sh install test 1.1
+# Test without parameter
+if RESULT=$(./mason.sh install test 1.1 --header-only 2>&1); then
+    [ $? != 0 ] # should not return exit code 0
+else
+    [ "${RESULT}" = $'[Mason] Downloading package http://localhost:8000/test/packages/headers/test/1.1.tar.gz...
+[Mason] curl: (22) The requested URL returned error: 404 Not Found' ]
+fi
 
 # Check that folders don't exist
 mason_succeeds "cached tarball" [ ! -f "mason_packages/.binaries/${TEST_SLUG}/test/1.0.tar.gz" ]

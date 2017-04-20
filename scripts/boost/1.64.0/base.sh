71fe11ffd696200706cce11cf59a1d984e991863
Dane Springmeyer <dane@mapbox.com>
2017-04-19 17:42:08 -0700

    boost-1.64.0

---
+++ scripts/boost/1.64.0/base.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# NOTE: use the ./utils/new_boost.sh script to create new versions
+
+export MASON_VERSION=1.64.0
+export BOOST_VERSION=${MASON_VERSION//./_}
+export BOOST_TOOLSET="clang"
+export BOOST_TOOLSET_CXX="clang++"
+export BOOST_ARCH="x86"
+export BOOST_SHASUM=6e4dad39f14937af73ace20d2279e2468aad14d8
+# special override to ensure each library shares the cached download
+export MASON_DOWNLOAD_SLUG="boost-${MASON_VERSION}"

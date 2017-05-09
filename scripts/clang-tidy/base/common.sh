8518d6645b9d481d6ddbb5a54fba8c6b5e55c0fc
Bruno de Oliveira Abinader <bruno@mapbox.com>
2017-05-09 11:21:34 +0300

    Added tidy-related share files to clang-tidy [skip ci]

--- scripts/clang-tidy/base/common.sh
+++ scripts/clang-tidy/base/common.sh
@@ -7,6 +7,7 @@ function mason_build {
     # copy bin
     mkdir -p "${MASON_PREFIX}/bin"
     cp "${CLANG_PREFIX}/bin/${MASON_NAME}" "${MASON_PREFIX}/bin/"
+    cp "${CLANG_PREFIX}/bin/clang-apply-replacements" "${MASON_PREFIX}/bin/"
 
     # copy include/c++
     mkdir -p "${MASON_PREFIX}/include"
@@ -20,6 +21,13 @@ function mason_build {
     mkdir -p "${MASON_PREFIX}/lib"
     mkdir -p "${MASON_PREFIX}/lib/clang"
     cp -r ${CLANG_PREFIX}/lib/clang/${MASON_VERSION} "${MASON_PREFIX}/lib/clang/"
+
+    # copy tidy-related share files
+    mkdir -p "${MASON_PREFIX}/share"
+    cp -r "${CLANG_PREFIX}/share/clang/run-clang-tidy.py" "${MASON_PREFIX}/share/"
+    cp -r "${CLANG_PREFIX}/share/clang/clang-tidy-diff.py" "${MASON_PREFIX}/share/"
+    cp -r "${CLANG_PREFIX}/share/clang/run-find-all-symbols.py" "${MASON_PREFIX}/share/"
+
 }
 
 function mason_cflags {

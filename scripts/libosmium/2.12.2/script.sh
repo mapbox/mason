547cacb52b746a91516dfec65c573239dbf3f7cb
Dane Springmeyer <dane@mapbox.com>
2017-05-16 11:25:08 -0700

    Add libosmium 2.12.2

---
+++ scripts/libosmium/2.12.2/script.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+MASON_NAME=libosmium
+MASON_VERSION=2.12.2
+MASON_HEADER_ONLY=true
+
+. ${MASON_DIR}/mason.sh
+
+function mason_load_source {
+    mason_download \
+        https://github.com/osmcode/${MASON_NAME}/archive/v${MASON_VERSION}.tar.gz \
+        80d373117f1fc1722889cd4f1f81621d3c6476bf
+
+    mason_extract_tar_gz
+
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/${MASON_NAME}-${MASON_VERSION}
+}
+
+function mason_compile {
+    mkdir -p ${MASON_PREFIX}/include/
+    cp -r include/osmium ${MASON_PREFIX}/include/osmium
+}
+
+function mason_cflags {
+    echo "-I${MASON_PREFIX}/include"
+}
+
+function mason_ldflags {
+    :
+}
+
+mason_run "$@"

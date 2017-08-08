2d7075d57875899a14d8853efc19fbcb38ffc48e
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2017-08-08 14:35:20 +0300

    vector-tile 1.0.0-rc7
    Add new version with better compatibility with old compilers.

---
+++ scripts/vector-tile/1.0.0-rc7/script.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+MASON_NAME=vector-tile
+MASON_VERSION=1.0.0-rc7
+MASON_HEADER_ONLY=true
+
+. ${MASON_DIR}/mason.sh
+
+function mason_load_source {
+    mason_download \
+        https://github.com/mapbox/${MASON_NAME}/archive/v${MASON_VERSION}.tar.gz \
+        2af919fa4565f12e70f11087f59b34c80aa47a47
+
+    mason_extract_tar_gz
+
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/${MASON_NAME}-${MASON_VERSION}
+}
+
+function mason_compile {
+    mkdir -p ${MASON_PREFIX}/include/
+    cp -r include/mapbox ${MASON_PREFIX}/include/mapbox
+}
+
+function mason_cflags {
+    echo "-I${MASON_PREFIX}/include"
+}
+
+function mason_ldflags {
+    :
+}
+
+mason_run "$@"

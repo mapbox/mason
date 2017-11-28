bda1ef10cb05772066f3532e6f93b1055feee890
ingalls <ingalls@protonmail.com>
2017-11-28 10:24:04 -0500

    Add JQ Build script

---
+++ scripts/jq/1.5-239278fd/script.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+MASON_NAME=jq
+MASON_VERSION=239278fd3a02dc1ae0521a7a57d573dbf977b2d9
+MASON_LIB_FILE=bin/jq
+
+. ${MASON_DIR}/mason.sh
+
+function mason_load_source {
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/${MASON_NAME}-${MASON_VERSION}
+
+    git clone git://github.com/stedolan/${MASON_NAME}.git ${MASON_BUILD_PATH}
+    cd ${MASON_BUILD_PATH}
+    git checkout ${MASON_VERSION}
+
+    cd ../
+}
+
+function mason_compile {
+    git submodule update --init
+    autoreconf -fi
+
+    ./configure \
+        --prefix ${MASON_PREFIX} \
+        --with-oniguruma=builtin \
+        --disable-maintainer-mode
+
+    make -j${MASON_CONCURRENCY} LDFLAGS=-all-static
+
+    make install
+}
+
+function mason_cflags {
+    :
+}
+
+function mason_ldflags {
+    :
+}
+
+function mason_clean {
+    make clean
+}
+
+mason_run "$@"

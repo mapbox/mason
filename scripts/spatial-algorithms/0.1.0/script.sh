329a044b17c286ed524173ed4357278a8337a029
Dane Springmeyer <dane@mapbox.com>
2018-02-02 09:32:46 -0500

    fix link/hash/unpacked path for official tarball release

--- scripts/spatial-algorithms/0.1.0/script.sh
+++ scripts/spatial-algorithms/0.1.0/script.sh
@@ -8,12 +8,12 @@ MASON_HEADER_ONLY=true
 
 function mason_load_source {
     mason_download \
-        https://github.com/mapbox/${MASON_NAME}/tarball/v${MASON_VERSION} \
-        4bddd38bd4534aef9e1b10dfec601c733876bbd4
+        https://github.com/mapbox/${MASON_NAME}/archive/v${MASON_VERSION}.tar.gz \
+        502cec856118c76d5c8e38d6a51cce9e2d2a42c8
 
     mason_extract_tar_gz
 
-    export MASON_BUILD_PATH=${MASON_ROOT}/.build/mapbox-${MASON_NAME}-${MASON_VERSION}
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/${MASON_NAME}-${MASON_VERSION}
 }
 
 function mason_compile {

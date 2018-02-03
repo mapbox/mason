247586ed1653d8c9313fa94865beebfd82a39a26
Dane Springmeyer <dane@mapbox.com>
2018-02-03 11:23:25 -0500

    Add libedit 3.1

---
+++ scripts/libedit/3.1/script.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+MASON_NAME=libedit
+MASON_VERSION=3.1
+MASON_LIB_FILE=lib/libedit.a
+
+. ${MASON_DIR}/mason.sh
+
+function mason_load_source {
+    mason_download \
+        https://thrysoee.dk/editline/libedit-20170329-${MASON_VERSION}.tar.gz \
+        7e64a1cfa3f16e7fa854e0c8cc3756ce7b793919
+
+    mason_extract_tar_gz
+
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/libedit-20170329-${MASON_VERSION}
+}
+
+function mason_compile {
+    # Add optimization flags since CFLAGS overrides the default (-g -O2)
+    export CFLAGS="${CFLAGS} -O3 -DNDEBUG"
+    ./configure \
+        --prefix=${MASON_PREFIX} \
+        --enable-static \
+        --disable-shared \
+        --disable-dependency-tracking
+
+    make -j${MASON_CONCURRENCY}
+    make install
+}
+
+function mason_cflags {
+    echo -I${MASON_PREFIX}/include
+}
+
+function mason_ldflags {
+    :
+}
+
+function mason_static_libs {
+    echo ${MASON_PREFIX}/${MASON_LIB_FILE}
+}
+
+function mason_clean {
+    make clean
+}
+
+mason_run "$@"

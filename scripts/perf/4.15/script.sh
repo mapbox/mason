48be7f83a9db5663058387ecc22bdff4930fb43f
Dane Springmeyer <dane@mapbox.com>
2018-02-03 10:43:55 -0500

    latest binutils with libiberty + perf configure fixes

--- scripts/perf/4.15/script.sh
+++ scripts/perf/4.15/script.sh
@@ -23,16 +23,16 @@ function mason_prepare_compile {
     MASON_ZLIB=$(${MASON_DIR}/mason prefix zlib 1.2.8)
     ${MASON_DIR}/mason install xz 5.2.3
     MASON_XZ=$(${MASON_DIR}/mason prefix xz 5.2.3)
-    ${MASON_DIR}/mason install binutils 2.27
-    MASON_BINUTILS=$(${MASON_DIR}/mason prefix binutils 2.27)
+    ${MASON_DIR}/mason install binutils 2.30
+    MASON_BINUTILS=$(${MASON_DIR}/mason prefix binutils 2.30)
     ${MASON_DIR}/mason install slang 2.3.1
     MASON_SLANG=$(${MASON_DIR}/mason prefix slang 2.3.1)
     ${MASON_DIR}/mason install bzip2 1.0.6
     MASON_BZIP2=$(${MASON_DIR}/mason prefix bzip2 1.0.6)
     ${MASON_DIR}/mason install elfutils 0.168
     MASON_ELFUTILS=$(${MASON_DIR}/mason prefix elfutils 0.168)
-    EXTRA_CFLAGS="-m64 -I${MASON_SLANG}/include -I${MASON_ZLIB}/include  -I${MASON_XZ}/include -I${MASON_BINUTILS}/include -I${MASON_BZIP2}/include -I${MASON_ELFUTILS}/include"
-    EXTRA_LDFLAGS="-L${MASON_BZIP2}/lib -L${MASON_ZLIB}/lib -L${MASON_XZ}/lib -L${MASON_SLANG}/lib  -L${MASON_ELFUTILS}/lib -L${MASON_BINUTILS}/lib"
+    EXTRA_CFLAGS="-m64 -I${MASON_SLANG}/include -I${MASON_ZLIB}/include -I${MASON_XZ}/include -I${MASON_BINUTILS}/include -I${MASON_BZIP2}/include -I${MASON_ELFUTILS}/include"
+    EXTRA_LDFLAGS="-L${MASON_BZIP2}/lib -L${MASON_ZLIB}/lib -L${MASON_XZ}/lib -L${MASON_SLANG}/lib -L${MASON_ELFUTILS}/lib -L${MASON_BINUTILS}/lib"
 }
 
 # https://perf.wiki.kernel.org/index.php/Jolsa_Howto_Install_Sources
@@ -41,13 +41,14 @@ function mason_prepare_compile {
 # https://software.intel.com/en-us/articles/linux-perf-for-intel-vtune-Amplifier-XE
 function mason_compile {
     cd tools/perf
-    # hack to enable libdw since I could not get the feature checks to work to autodetect libdw correctly
-    patch -N -p1 < ${MASON_DIR}/scripts/${MASON_NAME}/${MASON_VERSION}/patch.diff
     # we set NO_LIBUNWIND since libdw is used from elfutils which is faster: https://lwn.net/Articles/579508/
     # note: LIBELF is needed for symbols + node --perf_basic_prof_only_functions
+    mkdir -p output-dir
+    rm -rf output-dir/*
     make \
-      LIBDW_LDFLAGS="-L${MASON_ELFUTILS}/lib -Wl,--start-group -lelf -lebl -lz -llzma -lbz2" \
-      LIBDW_CFLAGS="-I${MASON_ELFUTILS}/include" \
+      O=output-dir \
+      LIBDW_LDFLAGS="-L${MASON_ELFUTILS}/lib -Wl,--start-group -ldw -lelf -lebl -llzma -lz -lbz2 -ldl -L${MASON_BZIP2}/lib -L${MASON_XZ}/lib" \
+      LIBDW_CFLAGS="-I${MASON_ELFUTILS}/include/" \
       V=1 VF=1 \
       prefix=${MASON_PREFIX} \
       NO_LIBNUMA=1 \
@@ -58,7 +59,7 @@ function mason_compile {
       NO_LIBCRYPTO=1 \
       NO_LIBPERL=1 \
       NO_GTK2=1 \
-      LDFLAGS="${EXTRA_LDFLAGS}" \
+      LDFLAGS="${EXTRA_LDFLAGS} -Wl,--start-group -L${MASON_BINUTILS}/lib -lbfd -lopcodes -lelf -lz" \
       NO_LIBPYTHON=1 \
       WERROR=0 \
       EXTRA_CFLAGS="${EXTRA_CFLAGS}" \

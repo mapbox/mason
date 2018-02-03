391d55d31cf692681547ff45fbc5ee560008771f
Dane Springmeyer <dane@mapbox.com>
2018-02-03 11:06:24 -0500

    upgrade elfutils

--- scripts/perf/4.15/script.sh
+++ scripts/perf/4.15/script.sh
@@ -29,8 +29,8 @@ function mason_prepare_compile {
     MASON_SLANG=$(${MASON_DIR}/mason prefix slang 2.3.1)
     ${MASON_DIR}/mason install bzip2 1.0.6
     MASON_BZIP2=$(${MASON_DIR}/mason prefix bzip2 1.0.6)
-    ${MASON_DIR}/mason install elfutils 0.168
-    MASON_ELFUTILS=$(${MASON_DIR}/mason prefix elfutils 0.168)
+    ${MASON_DIR}/mason install elfutils 0.170
+    MASON_ELFUTILS=$(${MASON_DIR}/mason prefix elfutils 0.170)
     EXTRA_CFLAGS="-m64 -I${MASON_SLANG}/include -I${MASON_ZLIB}/include -I${MASON_XZ}/include -I${MASON_BINUTILS}/include -I${MASON_BZIP2}/include -I${MASON_ELFUTILS}/include"
     EXTRA_LDFLAGS="-L${MASON_BZIP2}/lib -L${MASON_ZLIB}/lib -L${MASON_XZ}/lib -L${MASON_SLANG}/lib -L${MASON_ELFUTILS}/lib -L${MASON_BINUTILS}/lib"
 }
@@ -39,6 +39,7 @@ function mason_prepare_compile {
 # https://askubuntu.com/questions/50145/how-to-install-perf-monitoring-tool/306683
 # https://www.spinics.net/lists/linux-perf-users/msg03040.html
 # https://software.intel.com/en-us/articles/linux-perf-for-intel-vtune-Amplifier-XE
+# see the readme.md in this directory for a log of what perf features are enabled
 function mason_compile {
     cd tools/perf
     # we set NO_LIBUNWIND since libdw is used from elfutils which is faster: https://lwn.net/Articles/579508/

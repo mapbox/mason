680c6a6c17c5902f9b0594b07470bb802db70291
Sam Matthews <matthews.sam@gmail.com>
2018-02-21 08:20:38 -0800

    vector tile 2.x f4728da (#557)

---
+++ scripts/vector-tile/f4728da/script.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+MASON_NAME=vector-tile
+MASON_VERSION=f4728da
+MASON_HEADER_ONLY=true
+
+. ${MASON_DIR}/mason.sh
+
+function mason_load_source {
+    mason_download \
+        https://github.com/mapbox/${MASON_NAME}/tarball/${MASON_VERSION} \
+        b2238cefa136756b8670f1577082bf03cc6a52ef
+
+    mason_extract_tar_gz
+
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/mapbox-${MASON_NAME}-${MASON_VERSION}
+}
+
+function mason_compile {
+    mkdir -p ${MASON_PREFIX}/include/
+    cp -r include/mapbox ${MASON_PREFIX}/include/mapbox
+}
+
+function mason_cflags {
+    echo "-I${MASON_PREFIX}/include"
+}
+
+function mason_ldflags {
+    :
+}
+
+mason_run "$@"

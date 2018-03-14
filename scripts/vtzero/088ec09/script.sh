e80e18346f07df837c92b4ac2e648910b3842795
Sam Matthews <matthews.sam@gmail.com>
2018-03-14 09:58:11 -0700

    latest vtzero (#564)

---
+++ scripts/vtzero/088ec09/script.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+MASON_NAME=vtzero
+MASON_VERSION=088ec09
+MASON_HEADER_ONLY=true
+
+. ${MASON_DIR}/mason.sh
+
+function mason_load_source {
+    mason_download \
+        https://github.com/mapbox/vtzero/tarball/${MASON_VERSION} \
+        6add6e71c2e82e204d281e0ac1f1b4a84a480d33
+
+    mason_extract_tar_gz
+
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/mapbox-vtzero-${MASON_VERSION}
+}
+
+function mason_compile {
+    mkdir -p ${MASON_PREFIX}/include/
+    cp -r include/vtzero ${MASON_PREFIX}/include/vtzero
+}
+
+function mason_cflags {
+    echo "-I${MASON_PREFIX}/include"
+}
+
+function mason_ldflags {
+    :
+}
+
+
+mason_run "$@"

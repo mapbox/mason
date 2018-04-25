30234b9817a73b43eb16af599c73160480ead6e4
Dane Springmeyer <dane@mapbox.com>
2018-04-24 17:18:29 -0700

    attempt (not working yet) to get cross compiling working on android

--- scripts/libzip/1.5.1/script.sh
+++ scripts/libzip/1.5.1/script.sh
@@ -20,24 +20,40 @@ function mason_load_source {
 function mason_prepare_compile {
     CMAKE_VERSION=3.8.2
     CCACHE_VERSION=3.3.1
-    ${MASON_DIR}/mason install cmake ${CMAKE_VERSION}
-    MASON_CMAKE=$(${MASON_DIR}/mason prefix cmake ${CMAKE_VERSION})
-    ${MASON_DIR}/mason install ccache ${CCACHE_VERSION}
-    MASON_CCACHE=$(${MASON_DIR}/mason prefix ccache ${CCACHE_VERSION})
+    if [[ ${MASON_PLATFORM} == 'android' ]] || [[ ${MASON_PLATFORM} == 'ios' ]]; then
+        MASON_PLATFORM= MASON_PLATFORM_VERSION= ${MASON_DIR}/mason install cmake ${CMAKE_VERSION}
+        MASON_CMAKE=$(MASON_PLATFORM= MASON_PLATFORM_VERSION= ${MASON_DIR}/mason prefix cmake ${CMAKE_VERSION})
+        ${MASON_DIR}/mason install ccache ${CCACHE_VERSION}
+        MASON_PLATFORM= MASON_PLATFORM_VERSION= MASON_CCACHE=$(MASON_PLATFORM= MASON_PLATFORM_VERSION= ${MASON_DIR}/mason prefix ccache ${CCACHE_VERSION})
+    fi
 }
 
 function mason_compile {
     mkdir -p ./build
     rm -rf ./build/*
     cd build
-    ${MASON_CMAKE}/bin/cmake ../ \
-        -DCMAKE_INSTALL_PREFIX=${MASON_PREFIX} \
-        -DZIP_STATIC=ON -DBUILD_SHARED_LIBS=OFF \
-        -DCMAKE_CXX_COMPILER_LAUNCHER="${MASON_CCACHE}/bin/ccache" \
-        -DCMAKE_BUILD_TYPE=Release \
-        ..
+
+    if [ ${MASON_PLATFORM} = 'android' ]; then
+        ${MASON_DIR}/utils/android.sh > toolchain.cmake
+
+        ${MASON_CMAKE}/bin/cmake ../ \
+            -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake \
+            -DCMAKE_INSTALL_PREFIX=${MASON_PREFIX} \
+            -DZIP_STATIC=ON -DBUILD_SHARED_LIBS=OFF \
+            -DCMAKE_CXX_COMPILER_LAUNCHER="${MASON_CCACHE}/bin/ccache" \
+            -DCMAKE_BUILD_TYPE=Release \
+            ..
+    else
+        ${MASON_CMAKE}/bin/cmake ../ \
+            -DCMAKE_INSTALL_PREFIX=${MASON_PREFIX} \
+            -DZIP_STATIC=ON -DBUILD_SHARED_LIBS=OFF \
+            -DCMAKE_CXX_COMPILER_LAUNCHER="${MASON_CCACHE}/bin/ccache" \
+            -DCMAKE_BUILD_TYPE=Release \
+            ..
+    fi
+ 
     make VERBOSE=1 -j${MASON_CONCURRENCY}
-    make install -j${MASON_CONCURRENCY}
+    make install
 }
 
 mason_run "$@"

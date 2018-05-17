54bc3847275ba72464a9afcecae3bba3faa6ec46
Daniel Patterson <danpat@danpat.net>
2018-05-16 17:05:43 -0700

    WIP public Valhalla 2.4.9

---
+++ scripts/valhalla/2.4.9/script.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+MASON_NAME=valhalla
+MASON_VERSION=2.4.9
+MASON_LIB_FILE=lib/libpng.a
+#MASON_PKGCONFIG_FILE=lib/pkgconfig/libpng.pc
+
+
+. ${MASON_DIR}/mason.sh
+
+function mason_load_source {
+#    mason_download \
+#        https://github.com/valhalla/${MASON_NAME}/archive/${MASON_VERSION}.tar.gz \
+#        12718a7f8d26f707469895fb2a7e69f748356f7d
+#
+#    mason_extract_tar_gz
+
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/${MASON_NAME}-${MASON_VERSION}
+
+    if [ ! -d "$MASON_BUILD_PATH" ] ; then
+        mkdir -p "$MASON_BUILD_PATH"
+        git clone --branch "$MASON_VERSION" --recursive https://github.com/valhalla/valhalla.git "$MASON_BUILD_PATH"
+    fi
+}
+
+function mason_prepare_compile {
+    # Install the zlib dependency when cross compiling as usually the host system only
+    # provides the zlib headers and libraries in the path for the host architecture.
+    if [ ${MASON_PLATFORM_VERSION} == "cortex_a9" ] || [ ${MASON_PLATFORM_VERSION} == "i686" ]; then
+        cd $(dirname ${MASON_ROOT})
+        ${MASON_DIR}/mason install zlib_shared ${ZLIB_SHARED_VERSION}
+        ${MASON_DIR}/mason link zlib_shared ${ZLIB_SHARED_VERSION}
+
+        MASON_ZLIB_CFLAGS="$(${MASON_DIR}/mason cflags zlib_shared ${ZLIB_SHARED_VERSION})"
+        MASON_ZLIB_LDFLAGS="-L$(${MASON_DIR}/mason prefix zlib_shared ${ZLIB_SHARED_VERSION})/lib"
+    fi
+
+    ${MASON_DIR}/mason install protobuf 3.5.1
+    ${MASON_DIR}/mason link protobuf 3.5.1
+
+    ${MASON_DIR}/mason install boost 1.66.0
+    ${MASON_DIR}/mason link boost 1.66.0
+    ${MASON_DIR}/mason install boost_libprogram_options 1.66.0
+    ${MASON_DIR}/mason link boost_libprogram_options 1.66.0
+    ${MASON_DIR}/mason install boost_libsystem 1.66.0
+    ${MASON_DIR}/mason link boost_libsystem 1.66.0
+    ${MASON_DIR}/mason install boost_libthread 1.66.0
+    ${MASON_DIR}/mason link boost_libthread 1.66.0
+    ${MASON_DIR}/mason install boost_libfilesystem 1.66.0
+    ${MASON_DIR}/mason link boost_libfilesystem 1.66.0
+    ${MASON_DIR}/mason install boost_libregex 1.66.0
+    ${MASON_DIR}/mason link boost_libregex 1.66.0
+    ${MASON_DIR}/mason install boost_libdate_time 1.66.0
+    ${MASON_DIR}/mason link boost_libdate_time 1.66.0
+    ${MASON_DIR}/mason install boost_libiostreams 1.66.0
+    ${MASON_DIR}/mason link boost_libiostreams 1.66.0
+
+    ${MASON_DIR}/mason install lua 5.3.0
+    ${MASON_DIR}/mason link lua 5.3.0
+
+}
+
+function mason_compile {
+    export CXXFLAGS="-isystem ${MASON_ROOT}/.link/include ${CXXFLAGS:-}"
+    export CFLAGS="-isystem ${MASON_ROOT}/.link/include ${CFLAGS:-}"
+    export LDFLAGS="-L${MASON_ROOT}/.link/lib ${LDFLAGS:-}"
+    export PATH="${MASON_ROOT}/.link/bin:${PATH}"
+    export LUA="${MASON_ROOT}/.link/bin/lua"
+    export LUA_INCLUDE="-isystem ${MASON_ROOT}/.link/include"
+    export LUA_LIB="-llua"
+
+#declare -x MASON_BUILD_PATH="/Users/danpat/mapbox/mason/mason_packages/.build/valhalla-2.4.9"
+#declare -x MASON_DIR="/Users/danpat/mapbox/mason"
+#declare -x MASON_DYNLIB_SUFFIX="dylib"
+#declare -x MASON_HOST_ARG="--host=x86_64-apple-darwin"
+#declare -x MASON_PLATFORM_VERSION="x86_64"
+#declare -x MASON_ROOT="/Users/danpat/mapbox/mason/mason_packages"
+
+export
+
+    ./autogen.sh
+
+    ./configure \
+        --with-protobuf-includes="$MASON_ROOT/.link/include" \
+        --with-protobuf-libdir="$MASON_ROOT/.link/lib" \
+        --prefix="$MASON_PREFIX" \
+        ${MASON_HOST_ARG} \
+        --enable-static \
+        --with-pic \
+        --disable-shared \
+        --disable-dependency-tracking \
+        --disable-python-bindings
+
+    V=1 VERBOSE=1 make install -j${MASON_CONCURRENCY}
+}
+
+function mason_strip_ldflags {
+    shift # -L...
+    shift # -lpng16
+    echo "$@"
+}
+
+function mason_ldflags {
+    mason_strip_ldflags $(`mason_pkgconfig` --static --libs)
+}
+
+function mason_clean {
+    make clean
+}
+
+mason_run "$@"

6fdcd184668e8050436d05f896726e2b767cd72d
Vladimir Agafonkin <agafonkin@gmail.com>
2018-06-22 19:52:41 +0300

    Add geojsonvt v6.4.0 (#619)
    * add geojsonvt v6.4.0
    
    * fix geojsonvt to only run one job on travis

---
+++ scripts/geojsonvt/6.4.0/script.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+MASON_NAME=geojsonvt
+MASON_VERSION=6.4.0
+MASON_HEADER_ONLY=true
+
+. ${MASON_DIR}/mason.sh
+
+function mason_load_source {
+    mason_download \
+        https://github.com/mapbox/geojson-vt-cpp/archive/v${MASON_VERSION}.tar.gz \
+        a5ed84cb624ffc8430372e7920a673dd31536d69
+
+    mason_extract_tar_gz
+
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/geojson-vt-cpp-${MASON_VERSION}
+}
+
+function mason_compile {
+    mkdir -p ${MASON_PREFIX}/include/
+    cp -r include/mapbox ${MASON_PREFIX}/include/mapbox
+}
+
+function mason_cflags {
+    echo "-I${MASON_PREFIX}/include"
+}
+
+function mason_ldflags {
+    :
+}
+
+
+mason_run "$@"

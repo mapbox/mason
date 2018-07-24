7c10cca30b7d7d70872ac03b770ccbfc4a33f436
Sudarsana Babu Nagineni <sudarsana.babu@mapbox.com>
2018-07-24 08:47:23 +0300

    Add vector tile v1.0.2

---
+++ scripts/vector-tile/1.0.2/script.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+MASON_NAME=vector-tile
+MASON_VERSION=1.0.2
+MASON_HEADER_ONLY=true
+
+. ${MASON_DIR}/mason.sh
+
+function mason_load_source {
+    mason_download \
+        https://github.com/mapbox/${MASON_NAME}/archive/v${MASON_VERSION}.tar.gz \
+        3a88cf47e703a8d4ba88285a72a18911f62a7476
+
+    mason_extract_tar_gz
+
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/${MASON_NAME}-${MASON_VERSION}
+}
+
+function mason_compile {
+    mkdir -p ${MASON_PREFIX}/include/
+    cp -r include/mapbox ${MASON_PREFIX}/include/mapbox
+}
+
+function mason_cflags {
+    echo "-I${MASON_PREFIX}/include"
+}
+
+function mason_ldflags {
+    :
+}
+
+mason_run "$@"

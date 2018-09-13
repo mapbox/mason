858dfccf86879f9afa47cbcd17f887d62a7f9b00
Dane Springmeyer <dane@mapbox.com>
2018-09-13 07:37:50 -0700

    work harder to statically link all bpkg deps [skip ci]

--- scripts/build2/0.8.0/script.sh
+++ scripts/build2/0.8.0/script.sh
@@ -17,17 +17,14 @@ function mason_load_source {
 }
 
 function mason_compile {
-    # NOTE: build2 requires a c++17 capable compiler and it uses CXX11_ABI features in libstdc++ (so it must be build with _GLIBCXX_USE_CXX11_ABI=1)
+    # NOTE: build2 requires a c++17 capable compiler and it uses CXX11_ABI features in libstdc++ (so it must be built with _GLIBCXX_USE_CXX11_ABI=1)
     # Since we want the binaries to be portable to pre cxx11 abi machines, we statically link against libc++ instead of linking against libstdc++
     # note with clang 4.x will hit "header 'shared_mutex' not found and cannot be generated" because c++17 support is lacking
     LDFLAGS=""
     if [[ $(uname -s) == 'Linux' ]]; then
         LDFLAGS="-Wl,--start-group -lc++ -lc++abi -pthread -lrt"
     fi
-    perl -i -p -e "s/pthread/pthread -stdlib=libc++ ${LDFLAGS} /g;" ./build2/bootstrap.sh
-    perl -i -p -e "s/=static/=static config.bin.exe.lib=static config.cxx.coptions=-stdlib=libc++ config.cxx.loptions='${LDFLAGS}' /g;" ./build.sh
-    perl -i -p -e "s/suffix=-stage /suffix=-stage config.bin.lib=static config.bin.exe.lib=static config.cxx.coptions=-stdlib=libc++ config.cxx.loptions='${LDFLAGS}' /g;" ./build.sh
-    perl -i -p -e "s/coptions=-O3 /coptions=-O3 config.bin.lib=static config.bin.exe.lib=static config.cxx.coptions=-stdlib=libc++ config.cxx.loptions='${LDFLAGS}' /g;" ./build.sh
+    patch -N -p1 < ${MASON_DIR}/scripts/${MASON_NAME}/${MASON_VERSION}/patch.diff
     ./build.sh --install-dir ${MASON_PREFIX} --verbose 3 --sudo "" --trust yes ${CXX:-clang++} -O3 -stdlib=libc++ ${LDFLAGS}
 }
 

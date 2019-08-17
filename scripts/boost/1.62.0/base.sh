d185d191fe22d041b227fcc1db2e783d47544682
Mickey Rose <mickey.rose@seznam.cz>
2019-08-17 22:44:44 +0200

    another take on guessing BOOST_TOOLSET
        guess_compiler_name $CXX
    
    Function guess_compiler_name prints the "basename" of its first argument
    (or second argument if the first is ccache).
    Passing non-quoted $CXX takes care of word splitting.
    
    The "basename" is extracted with simple shell parameter expansion.
    It differs from how POSIX basename command works, but in good ways:
    
        guess_compiler_name x/  # prints ''
        command basename x/     # prints 'x'
    
        guess_compiler_name ''  # prints ''
        command basename ''     # ??
    
    POSIX leaves unspecified whether the resulting string is '.' or ''.
    
        guess_compiler_name     # prints ''
        command basename        # error: missing operand
    
    Parameter expansion just works, with basename command we would need to
    check that the compiler variable is set and non-null.

--- scripts/boost/1.62.0/base.sh
+++ scripts/boost/1.62.0/base.sh
@@ -2,10 +2,17 @@
 
 # NOTE: use the ./utils/new_boost.sh script to create new versions
 
+function guess_compiler_name() {
+    case $1 in
+        ccache| */ccache) shift;;
+    esac
+    printf '%s' "${1##*/}"
+}
+
 export MASON_VERSION=1.62.0
 export BOOST_VERSION=${MASON_VERSION//./_}
-export BOOST_TOOLSET=$(CC=${CC#ccache }; basename -- ${CC%% *})
-export BOOST_TOOLSET_CXX=$(CXX=${CXX#ccache }; basename -- ${CXX%% *})
+export BOOST_TOOLSET=$(guess_compiler_name $CC)
+export BOOST_TOOLSET_CXX=$(guess_compiler_name $CXX)
 export BOOST_ARCH="x86"
 export BOOST_SHASUM=f4151eec3e9394146b7bebcb17b83149de0a6c23
 # special override to ensure each library shares the cached download

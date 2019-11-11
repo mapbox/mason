380e79323a363c09aedc256e5070bdf03a1fbfec
Blake Thompson <blake@mapbox.com>
2019-11-11 16:00:22 -0600

    Update for different download version name

--- scripts/geometry/4f1b6e6/script.sh
+++ scripts/geometry/4f1b6e6/script.sh
@@ -2,6 +2,7 @@
 
 MASON_NAME=geometry
 MASON_VERSION=4f1b6e6
+MASON_VERSION_FULL=4f1b6e688e6766df8a9ae698a814718d4ebcfdb5
 MASON_HEADER_ONLY=true
 
 . ${MASON_DIR}/mason.sh
@@ -13,7 +14,7 @@ function mason_load_source {
 
     mason_extract_tar_gz
 
-    export MASON_BUILD_PATH=${MASON_ROOT}/.build/geometry.hpp-${MASON_VERSION}
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/geometry.hpp-${MASON_VERSION_FULL}
 }
 
 function mason_compile {

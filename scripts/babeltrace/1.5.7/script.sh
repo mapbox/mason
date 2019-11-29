ec033c0a0f3810a5788373de8926e5922e770148
Dane Springmeyer <dane@mapbox.com>
2019-11-29 13:16:04 -0800

    add new deps of recent perf

---
+++ scripts/babeltrace/1.5.7/script.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+MASON_NAME=babletrace
+MASON_VERSION=1.5.7
+MASON_LIB_FILE=lib/libbabeltrace.a
+MASON_PKGCONFIG_FILE=lib/pkgconfig/babeltrace.pc
+
+. ${MASON_DIR}/mason.sh
+
+function mason_load_source {
+    mason_download \
+        https://www.efficios.com/files/babeltrace/babeltrace-${MASON_VERSION}.tar.bz2 \
+        a41d581a2f36ef2b4c2440805ceef205f1a1fe58
+
+    mason_extract_tar_bz2
+
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/babeltrace-${MASON_VERSION}
+}
+
+function mason_prepare_compile {
+    ${MASON_DIR}/mason install popt 1.16
+    MASON_POPT=$(${MASON_DIR}/mason prefix popt 1.16)
+    # ${MASON_DIR}/mason install elfutils 0.178
+    # MASON_ELFUTILS=$(${MASON_DIR}/mason prefix elfutils 0.178)
+}
+
+
+function mason_compile {
+    export CFLAGS="${CFLAGS:-} -O3 -DNDEBUG -I${MASON_POPT}/include -I${MASON_ELFUTILS}/include"
+    export LDFLAGS="${LDFLAGS:-} -L${MASON_POPT}/lib -liconv -L${MASON_ELFUTILS}/lib"
+
+    ./configure \
+        --prefix=${MASON_PREFIX} \
+        ${MASON_HOST_ARG} \
+        --enable-static \
+        --with-pic \
+        --disable-shared \
+        --disable-dependency-tracking \
+        --disable-debug-info
+
+    V=1 VERBOSE=1 make install -j${MASON_CONCURRENCY}
+}
+
+function mason_strip_ldflags {
+    shift # -L...
+    shift # -lpng16
+    echo "$@"
+}
+
+function mason_ldflags {
+    mason_strip_ldflags $(`mason_pkgconfig` --static --libs)
+}
+
+function mason_clean {
+    make clean
+}
+
+mason_run "$@"
---
+++ scripts/popt/1.16/script.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+MASON_NAME=popt
+MASON_VERSION=1.16
+MASON_LIB_FILE=lib/libpopt.a
+MASON_PKGCONFIG_FILE=lib/pkgconfig/popt.pc
+
+. ${MASON_DIR}/mason.sh
+
+function mason_load_source {
+    mason_download \
+        ftp://anduin.linuxfromscratch.org/BLFS/popt/popt-${MASON_VERSION}.tar.gz \
+        598c52b4e1085e5e9e8323642ed73302b69cadc3
+
+    mason_extract_tar_gz
+
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/popt-${MASON_VERSION}
+}
+
+function mason_compile {
+    export CFLAGS="${CFLAGS:-} -O3 -DNDEBUG"
+
+    ./configure \
+        --prefix=${MASON_PREFIX} \
+        ${MASON_HOST_ARG} \
+        --enable-static \
+        --with-pic \
+        --disable-shared \
+        --disable-dependency-tracking
+
+    V=1 VERBOSE=1 make install -j${MASON_CONCURRENCY}
+}
+
+function mason_strip_ldflags {
+    shift # -L...
+    shift # -lpng16
+    echo "$@"
+}
+
+function mason_ldflags {
+    mason_strip_ldflags $(`mason_pkgconfig` --static --libs)
+}
+
+function mason_clean {
+    make clean
+}
+
+mason_run "$@"

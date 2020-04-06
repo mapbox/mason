84aa14dac99ea54d5ef183afb7c7b5175324384e
Dane Springmeyer <dane@mapbox.com>
2020-04-06 09:25:17 -0700

    disable errors and gl code

--- scripts/mbgl-core/1.5.1/script.sh
+++ scripts/mbgl-core/1.5.1/script.sh
@@ -2,6 +2,7 @@
 
 MASON_NAME=mbgl-core
 MASON_VERSION=1.5.1
+SHA=c829ea7
 MASON_LIB_FILE=lib/libmbgl-core.a
 
 . ${MASON_DIR}/mason.sh
@@ -12,7 +13,7 @@ function mason_load_source {
      if [[ ! -d ${MASON_BUILD_PATH} ]]; then
         git clone https://github.com/mapbox/mapbox-gl-native ${MASON_BUILD_PATH}
     fi
-    (cd ${MASON_BUILD_PATH} && git checkout maps-v${MASON_VERSION} && git submodule update --init --recursive)
+    (cd ${MASON_BUILD_PATH} && git fetch -v && git checkout ${SHA} && git submodule update --init --recursive)
 }
 
 function mason_prepare_compile {
@@ -28,13 +29,13 @@ function mason_prepare_compile {
 }
 
 function mason_compile {
-    # remove -Werror due to https://github.com/mapbox/mapbox-gl-native/issues/16376
-    perl -i -p -e "s/-Werror/-Wall/g;" CMakeLists.txt
     mkdir -p build
     cd build
     ${MASON_CMAKE}/bin/cmake ../ \
       -DCMAKE_INSTALL_PREFIX=${MASON_PREFIX} -DCMAKE_BUILD_TYPE=Release \
       -DMBGL_WITH_CORE_ONLY=ON \
+      -DMBGL_WITH_OPENGL=OFF \
+      -DMBGL_WITH_WERROR=OFF \
       -G Ninja -DCMAKE_MAKE_PROGRAM=${MASON_NINJA}/bin/ninja \
       -DCMAKE_CXX_COMPILER_LAUNCHER=${MASON_CCACHE}/bin/ccache \
       -DCMAKE_CXX_COMPILER="$CXX" \

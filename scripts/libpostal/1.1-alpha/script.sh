5eb81a0f46238801ba9e5fa32b78fcbbbda23875
manoharuss <manohar@mapbox.com>
2020-07-15 18:45:37 -0400

    Add libpostal installation in mason with a script.sh

---
+++ scripts/libpostal/1.1-alpha/script.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+MASON_NAME=libpostal
+MASON_VERSION=1.1-alpha
+MASON_LIB_FILE=bin/libpostal
+
+. ${MASON_DIR}/mason.sh
+
+function mason_load_source {
+    mason_download \
+        https://github.com/osmcode/${MASON_NAME}/archive/v${MASON_VERSION}.tar.gz \
+        43795a3d903991d3864926393af10c3ec31a161c
+
+    mason_extract_tar_gz
+
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/${MASON_NAME}-${MASON_VERSION}
+}
+
+function mason_prepare_compile {
+    CCACHE_VERSION=3.3.4
+    ${MASON_DIR}/mason install ccache ${CCACHE_VERSION}
+    MASON_CCACHE=$(${MASON_DIR}/mason prefix ccache ${CCACHE_VERSION})
+    ${MASON_DIR}/mason install cmake 3.15.2
+    ${MASON_DIR}/mason link cmake 3.15.2
+
+    yum install -y curl autoconf automake libtool python-devel pkgconfig geos geos-devel jq
+}
+
+function mason_compile {
+    rm -rf build
+    mkdir -p build
+    cd build
+    CMAKE_PREFIX_PATH=${MASON_ROOT}/.link \
+    ${MASON_ROOT}/.link/bin/cmake \
+        -DCMAKE_INSTALL_PREFIX=${MASON_PREFIX} \
+        -DCMAKE_CXX_COMPILER_LAUNCHER="${MASON_CCACHE}/bin/ccache" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DBoost_NO_SYSTEM_PATHS=ON \
+        -DBoost_USE_STATIC_LIBS=ON \
+        ..
+    # limit concurrency on travis to avoid heavy jobs being killed
+    if [[ ${TRAVIS_OS_NAME:-} ]]; then
+        make VERBOSE=1 -j4
+    else
+        make VERBOSE=1 -j${MASON_CONCURRENCY}
+    fi
+    make install
+
+}
+
+function mason_cflags {
+    :
+}
+
+function mason_ldflags {
+    :
+}
+
+function mason_static_libs {
+    :
+}
+
+mason_run "$@"

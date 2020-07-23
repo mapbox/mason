add138bc91527ce73a26a1255b542d8d868f8b42
manoharuss <manohar@mapbox.com>
2020-07-23 15:54:36 -0400

    cleanup

--- scripts/libpostal/1.1-alpha/script.sh
+++ scripts/libpostal/1.1-alpha/script.sh
@@ -21,17 +21,21 @@ function mason_prepare_compile {
     ${MASON_DIR}/mason install ccache ${CCACHE_VERSION}
     MASON_CCACHE=$(${MASON_DIR}/mason prefix ccache ${CCACHE_VERSION})
     ${MASON_DIR}/mason install cmake 3.15.2
+    MASON_CCACHE=$(${MASON_DIR}/mason prefix ccache ${CCACHE_VERSION})
+    ${MASON_DIR}/mason install cmake 3.15.2
     ${MASON_DIR}/mason link cmake 3.15.2
 
     # yum install curl autoconf automake libtool python-devel pkgconfig geos geos-devel jq
+
 }
 
 function mason_compile {
-    rm -rf build
-    mkdir -p build
-    cd build
-    CMAKE_PREFIX_PATH=${MASON_ROOT}/.link \
+    echo "LOG"
+    echo "mason prefix : " $MASON_PREFIX
+    echo "mason root : " $MASON_ROOT
+    echo "mason cache : " $MASON_CCACHE
 
+    CMAKE_PREFIX_PATH=${MASON_ROOT}/.link \
     ${MASON_ROOT}/.link/bin/cmake \
         -DCMAKE_INSTALL_PREFIX=${MASON_PREFIX} \
         -DCMAKE_CXX_COMPILER_LAUNCHER="${MASON_CCACHE}/bin/ccache" \
@@ -39,7 +43,6 @@ function mason_compile {
         -DBoost_NO_SYSTEM_PATHS=ON \
         -DBoost_USE_STATIC_LIBS=ON \
         ..
-
     # limit concurrency on travis to avoid heavy jobs being killed
     if [[ ${TRAVIS_OS_NAME:-} ]]; then
         make VERBOSE=1 -j4
@@ -47,7 +50,6 @@ function mason_compile {
         make VERBOSE=1 -j${MASON_CONCURRENCY}
     fi
     make install
-
 }
 
 function mason_cflags {

57bc741ee15a82da305af117c47a9b0472ebdaab
Artem Pavlenko <artem@mapnik.org>
2021-01-19 15:29:45 +0000

    Add proj 7.2.1

---
+++ scripts/proj/7.2.1/script.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+MASON_NAME=proj
+MASON_VERSION=7.2.1
+MASON_LIB_FILE=lib/libproj.a
+GRID_VERSION="1.8"
+
+. ${MASON_DIR}/mason.sh
+
+function mason_load_source {
+    mason_download \
+        https://download.osgeo.org/proj/proj-${MASON_VERSION}.tar.gz \
+        92b57d23bf86fc985bf33f5bf82b3ada0820cab0
+
+    mason_extract_tar_gz
+
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build/${MASON_NAME}-${MASON_VERSION}
+}
+
+function mason_prepare_compile {
+    ${MASON_DIR}/mason install sqlite 3.21.0
+    MASON_SQLITE=$(${MASON_DIR}/mason prefix sqlite 3.21.0)
+}
+
+function mason_compile {
+    #curl --retry 3 -f -# -L https://download.osgeo.org/proj/proj-datumgrid-${GRID_VERSION}.zip -o proj-datumgrid-${GRID_VERSION}.zip
+    #cd data
+    #unzip -o ../proj-datumgrid-${GRID_VERSION}.zip
+    #cd ../
+    export PATH="${MASON_ROOT}/.link/bin:${PATH}"
+    export PKG_CONFIG_PATH="${MASON_SQLITE}/lib/pkgconfig"
+    export CXXFLAGS="${CXXFLAGS} -O3 -DNDEBUG"
+    ./configure --prefix=${MASON_PREFIX} \
+    ${MASON_HOST_ARG} \
+    --enable-static \
+    --disable-shared \
+    --disable-dependency-tracking
+
+    make -j${MASON_CONCURRENCY}
+    make install
+}
+
+function mason_cflags {
+    echo -I${MASON_PREFIX}/include
+}
+
+function mason_ldflags {
+    echo "-lproj"
+}
+
+function mason_clean {
+    make clean
+}
+
+mason_run "$@"
